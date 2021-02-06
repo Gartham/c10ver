74236b6e57ea9e7c867daba4771dc6300855b520
Gartham <Gartham@dusttoash.org>
2021-02-06 04:31:26 -0600

    Setting Up

--- src/gartham/c10ver/Clover.java
+++ src/gartham/c10ver/Clover.java
@@ -1,7 +1,15 @@
 package gartham.c10ver;
 
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Clover {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws LoginException {
+		try (var s = new Scanner(Clover.class.getResourceAsStream("token.txt"))) {
+			JDABuilder.createLight(s.nextLine()).build();
+		}
 	}
 }
