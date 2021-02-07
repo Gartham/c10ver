8222075991051aa65219d1d6f1be51416945d8e3
Gartham <Gartham@dusttoash.org>
2021-02-07 11:00:09 -0600

    Event Listening

--- src/gartham/c10ver/Clover.java
+++ src/gartham/c10ver/Clover.java
@@ -9,7 +9,8 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Clover {
 	public static void main(String[] args) throws LoginException {
 		try (var s = new Scanner(Clover.class.getResourceAsStream("token.txt"))) {
-			JDABuilder.createLight(s.nextLine()).build();
+			var jda = JDABuilder.createLight(s.nextLine()).build();
+			jda.addEventListener(new CommandHandler());
 		}
 	}
 }
---
+++ src/gartham/c10ver/CommandHandler.java
@@ -0,0 +1,17 @@
+package gartham.c10ver;
+
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+
+public class CommandHandler implements EventListener {
+
+	@Override
+	public void onEvent(GenericEvent event) {
+		if (event instanceof MessageReceivedEvent) {
+			var mre = (MessageReceivedEvent) event;
+			System.out.println(mre.getMessage().getContentRaw());
+		}
+	}
+
+}
