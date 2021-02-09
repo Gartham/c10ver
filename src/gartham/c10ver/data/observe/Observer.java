d5cfc31a2a6a18847dd0602cdc068f636b8cfc54
Gartham <Gartham@dusttoash.org>
2021-02-09 14:48:56 -0600

    Remove stuff I just added lol

--- src/gartham/c10ver/data/observe/Observer.java
+++ src/gartham/c10ver/data/observe/Observer.java
@@ -2,8 +2,4 @@ package gartham.c10ver.data.observe;
 
 public interface Observer {
 	void observe();
-
-	default <V> PropertyObserver<V> toPropertyObserver() {
-		return (property, value) -> Observer.this.observe();
-	}
 }
--- src/gartham/c10ver/data/observe/PropertyObserver.java
+++
@@ -1,5 +0,0 @@
-package gartham.c10ver.data.observe;
-
-public interface PropertyObserver<V> {
-	void observe(Property<? extends V> property, V value);
-}
