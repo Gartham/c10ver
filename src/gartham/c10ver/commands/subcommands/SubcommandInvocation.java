92f8df6cc3c64a5d95460d6c23bbad3cc9c08057
Gartham <Gartham@dusttoash.org>
2021-02-19 21:47:31 -0600

    Create SubcommandInvocation.java

---
+++ src/gartham/c10ver/commands/subcommands/SubcommandInvocation.java
@@ -0,0 +1,20 @@
+package gartham.c10ver.commands.subcommands;
+
+import gartham.c10ver.commands.CommandInvocation;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class SubcommandInvocation extends CommandInvocation {
+
+	public final String[] preargs;
+
+	public SubcommandInvocation(String prefix, String cmdName, MessageReceivedEvent event, String[] preargs,
+			String... args) {
+		super(prefix, cmdName, event, args);
+		this.preargs = preargs;
+	}
+
+	public String[] getPreargs() {
+		return preargs;
+	}
+
+}
