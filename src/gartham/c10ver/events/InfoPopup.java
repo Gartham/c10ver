1fed89e87b491870f64b84c68acde166d0ae39ed
Gartham <Gartham@dusttoash.org>
2021-02-21 21:07:02 -0600

    Update InfoPopup.java

--- src/gartham/c10ver/events/InfoPopup.java
+++ src/gartham/c10ver/events/InfoPopup.java
@@ -4,4 +4,9 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public interface InfoPopup {
 	void show(MessageReceivedEvent event);
+
+	static InfoPopup of(String text) {
+		return event -> event.getChannel().sendMessage(text).queue();
+	}
+
 }
