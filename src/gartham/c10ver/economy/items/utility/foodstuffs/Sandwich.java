7705423b409f5bebd9462161b87d13ca2ea6b087
Gartham <Gartham@dusttoash.org>
2021-02-24 15:34:24 -0600

    Fix Sandw(h)ich class

---
+++ src/gartham/c10ver/economy/items/utility/foodstuffs/Sandwich.java
@@ -0,0 +1,29 @@
+package gartham.c10ver.economy.items.utility.foodstuffs;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.alixia.javalibrary.json.JSONObject;
+
+public class Sandwich extends Foodstuff {
+
+	public static final String ITEM_TYPE = "sandwich", ITEM_NAME = "Sandwich", ITEM_ICON = ":sandwich:";
+	public static final BigDecimal ITEM_MULT = BigDecimal.valueOf(25, 2);// 0.25 (25, shifted to the right twice.)
+	public static final BigInteger ITEM_TTL = sec(150);// 2.5min
+
+	{
+		setItemName(ITEM_NAME);
+		setIcon(ITEM_ICON);
+		setMultiplier(ITEM_MULT);
+		setTTL(ITEM_TTL);
+	}
+
+	public Sandwich(JSONObject properties) {
+		super(ITEM_TYPE, properties);
+	}
+
+	public Sandwich() {
+		super(ITEM_TYPE);
+	}
+
+}
