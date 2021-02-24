ed732540d90e66569c6ffb1ce84b902fa64a25f9
Wörk <izzie18.itb@gmail.com>
2021-02-24 20:29:32 +0000

    yeb

---
+++ src/gartham/c10ver/economy/items/utility/foodstuffs/Sandwhich.java
@@ -0,0 +1,20 @@
+package gartham.c10ver.economy.items.utility.foodstuffs;
+
+import org.alixia.javalibrary.json.JSONObject;
+
+public class Sandwhich extends Foodstuff{
+
+	
+	public static final String ITEM_TYPE ="sandwhich", ITEM_NAME = "Sandwhich", ITEM_ICON = ":sandwhich:";
+	
+	public Sandwhich(JSONObject properties) {
+		super(ITEM_TYPE, properties);
+		
+	}
+
+	public Sandwhich() {
+		super(ITEM_TYPE, ITEM_NAME, ITEM_ICON);
+
+	}
+
+}
