cedb7d92f0c506c88db82cf82792dd13ab57eaa0
Gartham <Gartham@dusttoash.org>
2021-02-24 15:29:09 -0600

    Update Spaghetti.java

--- src/gartham/c10ver/economy/items/utility/foodstuffs/Spaghetti.java
+++ src/gartham/c10ver/economy/items/utility/foodstuffs/Spaghetti.java
@@ -1,6 +1,7 @@
 package gartham.c10ver.economy.items.utility.foodstuffs;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 import org.alixia.javalibrary.json.JSONObject;
 
@@ -8,16 +9,20 @@ public class Spaghetti extends Foodstuff {
 
 	public static final String ITEM_TYPE = "spaghetti", ITEM_NAME = "Spaghetti", ITEM_ICON = ":spaghetti:";
 	public static final BigDecimal ITEM_MULT = BigDecimal.valueOf(5, 1);
+	public static final BigInteger ITEM_TTL = min(5);
+
+	{
+		setIcon(ITEM_ICON);
+		setItemName(ITEM_NAME);
+		setMultiplier(ITEM_MULT);
+	}
 
 	public Spaghetti() {
-		super(ITEM_TYPE, ITEM_NAME, ITEM_ICON);
-		setMultiplierValue(ITEM_MULT);
+		super(ITEM_TYPE);
 	}
 
 	public Spaghetti(JSONObject properties) {
 		super(ITEM_TYPE, properties);
-		setItemName(ITEM_NAME);
-		setIcon(ITEM_ICON);
 	}
 
 }
