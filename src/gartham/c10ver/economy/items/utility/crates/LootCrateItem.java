f4a4b1a23e4b74d5c3bf773b86f0d520e6bd50a6
Gartham <Gartham@dusttoash.org>
2021-02-25 13:13:21 -0600

    Update LootCrateItem.java

--- src/gartham/c10ver/economy/items/utility/crates/LootCrateItem.java
+++ src/gartham/c10ver/economy/items/utility/crates/LootCrateItem.java
@@ -6,39 +6,24 @@ import gartham.c10ver.economy.items.Item;
 
 public class LootCrateItem extends Item {
 
-	public static final String ITEM_TYPE = "loot-crate", ITEM_NAME = "Loot Crate";
+	public static final String ITEM_TYPE = "loot-crate";
+	private final Property<String> crateType = stringProperty("type");
 
-	protected final Property<CrateType> type = enumProperty("type", CrateType.DAILY, CrateType.class);
-
-	public CrateType getType() {
-		return type.get();
+	protected final void setCrateType(String type) {
+		crateType.set(type);
 	}
 
-	public enum CrateType {
-		DAILY("<:crate:808762616456675338>", "Daily"), WEEKLY("<:crate:808762616456675338>", "Weekly"),
-		MONTHLY("<:crate:808762616456675338>", "Monthly");
-
-		private final String icon, name;
-
-		private CrateType(String icon, String name) {
-			this.icon = icon;
-			this.name = name;
-		}
-
+	public String getCrateType() {
+		return crateType.get();
 	}
 
 	public LootCrateItem(JSONObject props) {
 		super(ITEM_TYPE, props);// Item class does a check on item property with JSON input.
-		load(type, props);
-		getIconProperty().set(getType().icon);
-		getItemNameProperty().set(ITEM_NAME);
-		setCustomName(getType().name + ' ' + ITEM_NAME);
+		load(crateType, props);
 	}
 
-	public LootCrateItem(CrateType type) {
-		super(ITEM_TYPE, ITEM_NAME, type.icon);
-		this.type.set(type);
-		setCustomName(getType().name + ' ' + ITEM_NAME);
+	public LootCrateItem() {
+		super(ITEM_TYPE);
 	}
 
 }
