bbf9bc2928a1c651565a46b0b466cc1b1d46ccff
Gartham <Gartham@dusttoash.org>
2021-02-25 14:45:10 -0600

    Update DailyCrate.java

--- src/gartham/c10ver/economy/items/utility/crates/DailyCrate.java
+++ src/gartham/c10ver/economy/items/utility/crates/DailyCrate.java
@@ -2,7 +2,6 @@ package gartham.c10ver.economy.items.utility.crates;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,8 +33,7 @@ public class DailyCrate extends LootCrateItem {
 		if (Math.random() < 0.1)
 			items.add(new ItemBunch<>(new Sandwich(), Math.random() < 0.2 ? BigInteger.TWO : BigInteger.ONE));
 		BigInteger cloves = BigInteger.valueOf((long) (Math.random() * 650 + 300));
-		return Math.random() < 0.01
-				? new Rewards(items, cloves, new Multiplier(Instant.now().plusSeconds(150), BigDecimal.valueOf(25, 2)))
+		return Math.random() < 0.01 ? new Rewards(items, cloves, Multiplier.ofSec(90, BigDecimal.valueOf(25, 2)))
 				: new Rewards(items, cloves);
 	}
 
