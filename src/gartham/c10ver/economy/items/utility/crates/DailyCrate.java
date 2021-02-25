8cae5afe3ec08a472ca58f299bccd60a7243f929
Gartham <Gartham@dusttoash.org>
2021-02-25 15:39:51 -0600

    Update DailyCrate.java

--- src/gartham/c10ver/economy/items/utility/crates/DailyCrate.java
+++ src/gartham/c10ver/economy/items/utility/crates/DailyCrate.java
@@ -13,7 +13,8 @@ import gartham.c10ver.economy.items.ItemBunch;
 import gartham.c10ver.economy.items.utility.foodstuffs.Sandwich;
 
 public class DailyCrate extends LootCrateItem {
-	public static final String ITEM_NAME = "Daily Crate", ITEM_ICON = "<:crate:808762616456675338>", CRATE_TYPE = "daily";
+	public static final String ITEM_NAME = "Daily Crate", ITEM_ICON = "<:crate:808762616456675338>",
+			CRATE_TYPE = "daily";
 
 	{
 		setItemName(ITEM_NAME);
@@ -31,8 +32,12 @@ public class DailyCrate extends LootCrateItem {
 	@Override
 	protected Rewards open() {
 		List<ItemBunch<?>> items = new ArrayList<>();
-		if (Math.random() < 0.1)
+		if (Math.random() < 0.05)
 			items.add(new ItemBunch<>(new Sandwich(), Math.random() < 0.2 ? BigInteger.TWO : BigInteger.ONE));
+		if (Math.random() < 0.1)
+			items.add(new ItemBunch<>(new DailyCrate(), BigInteger.valueOf(3)));
+		else if (Math.random() < 0.04)
+			items.add(new ItemBunch<>(new WeeklyCrate(), BigInteger.ONE));
 		BigInteger cloves = BigInteger.valueOf((long) (Math.random() * 650 + 300));
 		return Math.random() < 0.01 ? new Rewards(items, cloves, Multiplier.ofSec(90, BigDecimal.valueOf(25, 2)))
 				: new Rewards(items, cloves);
