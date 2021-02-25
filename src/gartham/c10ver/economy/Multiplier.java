5c8a3984bf88aa7cb8ce28a5637978ae0d24bf5e
Gartham <Gartham@dusttoash.org>
2021-02-25 14:44:34 -0600

    Update Multiplier.java

--- src/gartham/c10ver/economy/Multiplier.java
+++ src/gartham/c10ver/economy/Multiplier.java
@@ -1,5 +1,8 @@
 package gartham.c10ver.economy;
 
+import static java.time.temporal.ChronoUnit.HOURS;
+import static java.time.temporal.ChronoUnit.MINUTES;
+
 import java.math.BigDecimal;
 import java.time.Instant;
 
@@ -7,10 +10,39 @@ import org.alixia.javalibrary.json.JSONObject;
 import org.alixia.javalibrary.json.JSONValue;
 
 import gartham.c10ver.data.PropertyObject;
+import static java.time.temporal.ChronoUnit.*;
 
 public final class Multiplier extends PropertyObject {
 	private final Property<Instant> expr = instantProperty("expr");
 	private final Property<BigDecimal> amt = bigDecimalProperty("amt");
+	
+	public static Multiplier ofSec(long secondsTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plusSeconds(secondsTillExpired), value);
+	}
+	
+	public static Multiplier ofMin(long minutesTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plus(minutesTillExpired, MINUTES), value);
+	}
+	
+	public static Multiplier ofHr(long hrsTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plus(hrsTillExpired, HOURS), value);
+	}
+	
+	public static Multiplier ofDay(long daysTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plus(daysTillExpired, DAYS), value);
+	}
+	
+	public static Multiplier ofWk(long weeksTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plus(weeksTillExpired, WEEKS), value);
+	}
+	
+	public static Multiplier ofMth(long monthsTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plus(monthsTillExpired, MONTHS), value);
+	}
+	
+	public static Multiplier ofYr(long yrsTillExpired, BigDecimal value) {
+		return new Multiplier(Instant.now().plus(yrsTillExpired, YEARS), value);
+	}
 
 	public Instant getExpiration() {
 		return expr.get();
