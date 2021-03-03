da11a5b8c7295cb899d1fa24522c3bec2b514b9d
Gartham <Gartham@dusttoash.org>
2021-03-03 17:43:35 -0600

    Update Transaction.java

--- src/gartham/c10ver/transactions/Transaction.java
+++ src/gartham/c10ver/transactions/Transaction.java
@@ -2,10 +2,24 @@ package gartham.c10ver.transactions;
 
 import java.math.BigDecimal;
 
+import org.alixia.javalibrary.json.JSONNumber;
+import org.alixia.javalibrary.json.JSONObject;
+import org.alixia.javalibrary.json.JSONValue;
+
 public class Transaction {
 
 	public enum Item {
-		GLOBAL_MULT, SERVER_MULT
+		GLOBAL_MULT, SERVER_MULT;
+
+		private static final Item[] VALUES = values();
+
+		public JSONValue toJSON() {
+			return new JSONNumber(ordinal());
+		}
+
+		public static Item valueOf(JSONValue json) {
+			return VALUES[((JSONNumber) json).intValue()];
+		}
 	}
 
 	private final Item item;
@@ -14,6 +28,20 @@ public class Transaction {
 	public Transaction(Item item, BigDecimal amount) {
 		this.item = item;
 		this.amount = amount;
+	}
+
+	public Transaction(JSONValue json) {
+		var o = (JSONObject) json;
+		item = Item.valueOf(o.get("item"));
+		amount = new BigDecimal(o.getString("amt"));
+	}
+
+	public JSONValue toJSON() {
+		JSONObject obj = new JSONObject();
+		obj.put("item", item.toJSON());
+		obj.put("amt", amount.toString());
+		return obj;
+
 	}
 
 }
