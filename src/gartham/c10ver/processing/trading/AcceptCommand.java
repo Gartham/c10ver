bec1c80f5ca90c3c825bb2bc7e0d8753c6fb4587
Gartham <Gartham@dusttoash.org>
2021-03-21 12:19:19 -0500

    Update AcceptCommand.java

--- src/gartham/c10ver/processing/trading/AcceptCommand.java
+++ src/gartham/c10ver/processing/trading/AcceptCommand.java
@@ -2,6 +2,10 @@ package gartham.c10ver.processing.trading;
 
 import gartham.c10ver.commands.CommandInvocation;
 import gartham.c10ver.commands.MatchBasedCommand;
+import gartham.c10ver.economy.User;
+import gartham.c10ver.economy.items.Inventory;
+import gartham.c10ver.economy.items.utils.ItemList;
+import gartham.c10ver.economy.items.utils.ItemList.Entry;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 public class AcceptCommand extends MatchBasedCommand {
@@ -14,23 +18,38 @@ public class AcceptCommand extends MatchBasedCommand {
 	}
 
 	@Override
-	public void exec(CommandInvocation inv) {
+	public void exec(CommandInvocation inv) {// TODO Synchronize
 		var person = trade.getParticipant(inv.event.getAuthor());
 		if (inv.args.length == 0)
 			if (!person.isFinished()) {
 				person.setFinished(true);
 				var other = trade.isRecipient(inv.event.getAuthor()) ? trade.getRequester() : trade.getRecip();
 				if (other.isFinished()) {
-					// TODO
-				} else {
+
+					// Do the actual tradeoff.
+
+					ItemList invokerList = person.getItems();
+					Inventory i = person.getEcouser().getInventory();
+					for (var e : invokerList.getItems()) {// For each item specified to trade, pull it out of the
+															// inventory.
+						var entry = i.get(e.getItem().getItemType());
+						if (entry == null) {
+							inv.event.getChannel()
+									.sendMessage("The trade could not be completed as requested because of ");// TODO
+						}
+					}
+				} else
 					inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention()
 							+ " you've accepted the trade. Now the person you're trading with needs to accept. If they make any changes, you'll have to accept again before the trade can take place.\n\n By the way, here are the items you're planning on trading:")
 							.embed(person.getTrade(new EmbedBuilder()).build()).queue();
-				}
 			} else
 				inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention()
 						+ " you've already accepted, you need to wait on the person you're trading with to accept.")
 						.queue();
+	}
+
+	private static void transfer(ItemList items, User from, User to) {
+		Inventory snapshot = from.getInventory().clone
 	}
 
 }
