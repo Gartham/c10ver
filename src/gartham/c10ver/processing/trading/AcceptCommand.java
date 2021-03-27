4e4f2ad5bea1eb360f6322f00458965ac755b4fb
Gartham <Gartham@dusttoash.org>
2021-03-27 06:07:58 -0500

    Implement minimal trade features
    This should finish up the minimum needed for the trade command to be complete.

--- src/gartham/c10ver/processing/trading/AcceptCommand.java
+++ src/gartham/c10ver/processing/trading/AcceptCommand.java
@@ -3,9 +3,10 @@ package gartham.c10ver.processing.trading;
 import gartham.c10ver.commands.CommandInvocation;
 import gartham.c10ver.commands.MatchBasedCommand;
 import gartham.c10ver.economy.User;
+import gartham.c10ver.economy.items.Item;
 import gartham.c10ver.economy.items.UserInventory;
+import gartham.c10ver.economy.items.UserInventory.UserEntry;
 import gartham.c10ver.economy.items.utils.ItemList;
-import gartham.c10ver.economy.items.utils.ItemList.Entry;
 import net.dv8tion.jda.api.EmbedBuilder;
 
 public class AcceptCommand extends MatchBasedCommand {
@@ -17,6 +18,7 @@ public class AcceptCommand extends MatchBasedCommand {
 		this.trade = trade;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public void exec(CommandInvocation inv) {// TODO Synchronize
 		var person = trade.getParticipant(inv.event.getAuthor());
@@ -28,16 +30,58 @@ public class AcceptCommand extends MatchBasedCommand {
 
 					// Do the actual tradeoff.
 
-					ItemList invokerList = person.getItems();
-					UserInventory i = person.getEcouser().getInventory();
+					ItemList invokerList = person.getItems(), otherList = other.getItems();
+					UserInventory takeInv = person.getEcouser().getInventory(),
+							recivInv = other.getEcouser().getInventory();
 					for (var e : invokerList.getItems()) {// For each item specified to trade, pull it out of the
 															// inventory.
-						var entry = i.get(e.getItem().getItemType());
+						var entry = takeInv.get(e.getItem().getItemType());
 						if (entry == null) {
-							inv.event.getChannel()
-									.sendMessage("The trade could not be completed as requested because of ");// TODO
+							inv.event.getChannel().sendMessage(
+									"The trade could not be completed as requested because of an error. (User did not have items to trade.)")
+									.queue();
+							return;
+						}
+
+						if (!((UserEntry<Item>) entry).has(e.getItem(), e.getCount())) {
+							inv.event.getChannel().sendMessage(
+									"The trade could not be completed as requested because of an error. (User did not have enough items to complete the trade.)")
+									.queue();
+							return;
+						}
+
+					}
+
+					for (var e : otherList.getItems()) {
+						var entry = recivInv.get(e.getItem().getItemType());
+						if (entry == null) {
+							inv.event.getChannel().sendMessage(
+									"The trade could not be completed as requested because of an error. (User did not have items to trade.)")
+									.queue();
+							return;
+						}
+						if (!((UserEntry<Item>) entry).has(e.getItem(), e.getCount())) {
+							inv.event.getChannel().sendMessage(
+									"The trade could not be completed as requested because of an error. (User did not have enough items to complete the trade.)")
+									.queue();
+							return;
 						}
 					}
+
+					for (var e : invokerList.getItems()) {
+						takeInv.remove(e.getItem(), e.getCount());
+						recivInv.add(e.getItem(), e.getCount());
+					}
+					for (var e : otherList.getItems()) {
+						recivInv.remove(e.getItem(), e.getCount());
+						takeInv.add(e.getItem(), e.getCount());
+					}
+
+					trade.getInitialChannel()
+							.sendMessage("Trade between " + trade.getRequesterUser().getAsMention() + " and "
+									+ trade.getRecipientUser().getAsMention()
+									+ " complete. Check your inventories! (`~inv`)")
+							.queue();
 				} else
 					inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention()
 							+ " you've accepted the trade. Now the person you're trading with needs to accept. If they make any changes, you'll have to accept again before the trade can take place.\n\n By the way, here are the items you're planning on trading:")
@@ -46,10 +90,6 @@ public class AcceptCommand extends MatchBasedCommand {
 				inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention()
 						+ " you've already accepted, you need to wait on the person you're trading with to accept.")
 						.queue();
-	}
-
-	private static void transfer(ItemList items, User from, User to) {
-//		Inventory snapshot = from.getInventory().clone
 	}
 
 }
