f0c0c81bfe100df01a5997b031e1d15809f9bbaa
Gartham <Gartham@dusttoash.org>
2021-04-01 16:32:23 -0500

    Create MathProblemGenerator.java

---
+++ src/gartham/c10ver/games/math/MathProblemGenerator.java
@@ -0,0 +1,7 @@
+package gartham.c10ver.games.math;
+
+import zeale.applicationss.notesss.utilities.generators.Generator;
+
+public interface MathProblemGenerator extends Generator<MathProblem> {
+
+}
