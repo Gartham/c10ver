d7ee6c06a27ada0c832116010f3bf2b3cb927136
Gartham <Gartham@dusttoash.org>
2021-04-01 11:09:32 -0500

    Update Main.java

--- utils/WordSorter/src/gartham/c10ver/utilityapps/wordsorter/Main.java
+++ utils/WordSorter/src/gartham/c10ver/utilityapps/wordsorter/Main.java
@@ -1,17 +1,27 @@
 package gartham.c10ver.utilityapps.wordsorter;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		var s = new Scanner(System.in);
 		while (s.hasNextLine()) {
-			sort(new File(s.nextLine()));
+			try {
+				sort(new File(s.nextLine()));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	private static void sort(File f) {
-
+	private static void sort(File f) throws IOException {
+		var st = new Splittree<>();
+		var sc = new Scanner(f);
+		while (sc.hasNextLine())
+			st.put(sc.nextLine(), true);
 	}
 }
