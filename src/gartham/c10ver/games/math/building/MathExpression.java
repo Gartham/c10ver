bd8e942454803e95495c27eec91d615300522dbe
Gartham <Gartham@dusttoash.org>
2021-04-03 16:46:48 -0500

    Update MathExpression.java

--- src/gartham/c10ver/games/math/building/MathExpression.java
+++ src/gartham/c10ver/games/math/building/MathExpression.java
@@ -8,12 +8,32 @@ import gartham.c10ver.games.math.MathUtils;
 public interface MathExpression extends MathProblem {
 	BigDecimal eval();
 
-	default int ord() {
+	default int lo() {
+		return -1;
+	}
+
+	default int ro() {
 		return -1;
 	}
 
 	@Override
 	default boolean check(String result) {
 		return MathUtils.check(eval(), result);
+	}
+
+	default CompoundMathExpression add(MathExpression second) {
+		return CompoundMathExpression.add(this, second);
+	}
+
+	default CompoundMathExpression subtract(MathExpression second) {
+		return CompoundMathExpression.subtract(this, second);
+	}
+
+	default CompoundMathExpression multiply(MathExpression second) {
+		return CompoundMathExpression.multiply(this, second);
+	}
+
+	default CompoundMathExpression divide(MathExpression second) {
+		return CompoundMathExpression.divide(this, second);
 	}
 }
