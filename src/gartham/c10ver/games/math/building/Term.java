c112c007d593b609df779b6ac9837d4d93cead44
Gartham <Gartham@dusttoash.org>
2021-04-03 04:43:51 -0500

    Update Term.java

--- src/gartham/c10ver/games/math/building/Term.java
+++ src/gartham/c10ver/games/math/building/Term.java
@@ -6,6 +6,22 @@ public final class Term implements MathExpression {
 
 	private final BigDecimal val;
 
+	public static Term of(String val) {
+		return new Term(new BigDecimal(val));
+	}
+
+	public static Term of(BigDecimal val) {
+		return new Term(val);
+	}
+
+	public static Term of(long val) {
+		return new Term(BigDecimal.valueOf(val));
+	}
+
+	public static Term of(double val) {
+		return new Term(BigDecimal.valueOf(val));
+	}
+
 	public Term(BigDecimal val) {
 		this.val = val;
 	}
