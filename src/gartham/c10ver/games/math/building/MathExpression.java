7bab1990cfe42c4801fde9006d8992bc6ddbbe4e
Gartham <Gartham@dusttoash.org>
2021-04-03 04:42:01 -0500

    Update MathExpression.java

--- src/gartham/c10ver/games/math/building/MathExpression.java
+++ src/gartham/c10ver/games/math/building/MathExpression.java
@@ -2,7 +2,18 @@ package gartham.c10ver.games.math.building;
 
 import java.math.BigDecimal;
 
-public interface MathExpression {
+import gartham.c10ver.games.math.MathProblem;
+import gartham.c10ver.games.math.MathUtils;
+
+public interface MathExpression extends MathProblem {
 	BigDecimal eval();
-	
+
+	default int ord() {
+		return -1;
+	}
+
+	@Override
+	default boolean check(String result) {
+		return MathUtils.check(eval(), result);
+	}
 }
