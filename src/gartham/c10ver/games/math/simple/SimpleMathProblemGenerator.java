8ddfd5c05eb6a50cdaa25ee107961ecffcab8f9e
Gartham <Gartham@dusttoash.org>
2021-04-04 11:57:46 -0500

    Update SimpleMathProblemGenerator.java

--- src/gartham/c10ver/games/math/simple/SimpleMathProblemGenerator.java
+++ src/gartham/c10ver/games/math/simple/SimpleMathProblemGenerator.java
@@ -2,56 +2,54 @@ package gartham.c10ver.games.math.simple;
 
 import java.util.Random;
 
-import gartham.c10ver.games.math.BasicMathProblem;
 import gartham.c10ver.games.math.MathProblemGenerator;
+import gartham.c10ver.games.math.building.MathExpression;
+import gartham.c10ver.games.math.building.Term;
 
 public class SimpleMathProblemGenerator implements MathProblemGenerator {
-	private Random random;
+	private Random rand;
 
 	public SimpleMathProblemGenerator(Random random) {
-		this.random = random;
+		this.rand = random;
 	}
 
 	public SimpleMathProblemGenerator() {
 		this(new Random());
 	}
 
-	public Random getRandom() {
-		return random;
+	public Random getRand() {
+		return rand;
 	}
 
 	public void setRandom(Random random) {
-		this.random = random;
+		this.rand = random;
 	}
 
 	@Override
-	public BasicMathProblem generate(double difficulty) {
+	public MathExpression generate(double difficulty) {
 		var sqrt = Math.sqrt(difficulty);
-		var tc = Math.max(0, Math.min(3, sqrt)) + 1;
-		System.out.println("SQRT: " + sqrt);
-		System.out.println("TERMCOUNT: " + tc);
+		var tc = Math.max(0, (int) Math.min(3, sqrt)) + 1;
 		var dis = sqrt * sqrt / 2;
-		System.out.println("DIS: " + dis);
 		var range = (int) (3.5 * (difficulty + 1 - dis));
 
-		StringBuilder sb = new StringBuilder();
-		int term = random.nextInt(range), result = term;
-		sb.append(term);
-
-		for (; tc > 0; tc--) {
-			switch ((int) (Math.random() * 4)) {
-			case 0:
-				
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + (int) (Math.random() * 4));
-			}
+		MathExpression expr = Term.of(rand.nextInt(3 * range));
+		while (tc-- > 0) {
+			Term n = Term.of(rand.nextInt(3 * range));
+			boolean shouldFlip = difficulty > 6 && rand.nextBoolean();
+			expr = switch (rand.nextInt(difficulty > 2 ? 3 : 2)) {
+			case 0 -> shouldFlip ? n.add(expr) : expr.add(n);
+			case 1 -> shouldFlip ? n.subtract(expr) : expr.subtract(n);
+			case 2 -> shouldFlip ? n.multiply(expr) : expr.multiply(n);
+			case 3 -> shouldFlip ? n.divide(expr) : expr.divide(n);
+			default -> throw new IllegalArgumentException("Unexpected value: " + rand.nextInt(difficulty > 2 ? 4 : 2));
+			};
 		}
-		return null;
+		return expr;
 	}
 
-	public static void main(String[] args) {
-		var smpg = new SimpleMathProblemGenerator();
-		System.out.println(smpg.next());
+	@Override
+	public MathExpression next() {
+		return (MathExpression) MathProblemGenerator.super.next();
 	}
 
 }
