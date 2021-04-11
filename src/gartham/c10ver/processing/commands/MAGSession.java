f840e6f23f19d41aee481e24b57f0c958cf3f093
Gartham <Gartham@dusttoash.org>
2021-04-11 15:58:22 -0500

    Create MAGSession.java

---
+++ src/gartham/c10ver/processing/commands/MAGSession.java
@@ -0,0 +1,12 @@
+package gartham.c10ver.processing.commands;
+
+/**
+ * A class used to manage the playing of a mining session. Stores state
+ * information of the current mining game session for a user.
+ * 
+ * @author Gartham
+ *
+ */
+class MAGSession {
+
+}
