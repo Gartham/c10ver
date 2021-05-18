b3dd998136fefdb2bf98dcc392bbe727028abd90
Gartham <Gartham@dusttoash.org>
2021-05-18 03:45:03 -0500

    Update TransactionHandler.java

--- src/gartham/c10ver/transactions/TransactionHandler.java
+++ src/gartham/c10ver/transactions/TransactionHandler.java
@@ -1,21 +1,20 @@
 package gartham.c10ver.transactions;
 
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 public abstract class TransactionHandler {
 
-	private Function<Transaction, TransactionResponse> transactionProcessor;
+	private Consumer<Transaction> transactionProcessor;
 
-	public final TransactionHandler setTransactionProcessor(
-			Function<Transaction, TransactionResponse> transactionProcessor) {
-		this.transactionProcessor = transactionProcessor;
+	public final TransactionHandler setTransactionProcessor(Consumer<Transaction> processor) {
+		transactionProcessor = processor;
 		return this;
 	}
 
 	public abstract void enable();
 
-	protected final TransactionResponse handleTransaction(Transaction transaction) {
-		return transactionProcessor.apply(transaction);
+	protected final void handleTransaction(Transaction transaction) {
+		transactionProcessor.accept(transaction);
 	}
 
 	public abstract void destroy();
