23c6c54aa3ab3fa18400d9d2064279be956b6b18
Gartham <Gartham@dusttoash.org>
2021-05-26 02:53:12 -0500

    Create AccoladeList.java

---
+++ src/gartham/c10ver/economy/accolades/AccoladeList.java
@@ -0,0 +1,13 @@
+package gartham.c10ver.economy.accolades;
+
+import java.io.File;
+
+import gartham.c10ver.data.autosave.SavablePropertyObject;
+
+public class AccoladeList extends SavablePropertyObject {
+	public AccoladeList(File file) {
+		super(file);
+	}
+	
+	
+}
