8fb01d2a08da3401b647714e87c7a0f2f5004e56
Gartham <Gartham@dusttoash.org>
2021-05-26 23:33:26 -0500

    Update AccoladeType.java

--- src/gartham/c10ver/economy/accolades/AccoladeType.java
+++ src/gartham/c10ver/economy/accolades/AccoladeType.java
@@ -1,12 +1,24 @@
 package gartham.c10ver.economy.accolades;
 
+import java.math.BigInteger;
+
 public enum AccoladeType {
 	;
-	private final String name, icon;
+	private final String name, icon, description;
+	private final BigInteger value;
 
-	private AccoladeType(String name, String icon) {
+	private AccoladeType(String name, String icon, String description, BigInteger value) {
 		this.name = name;
 		this.icon = icon;
+		this.description = description;
+		this.value = value;
+	}
+
+	private AccoladeType(String name, String icon, String description, long value) {
+		this.name = name;
+		this.icon = icon;
+		this.description = description;
+		this.value = BigInteger.valueOf(value);
 	}
 
 	public String getName() {
@@ -16,4 +28,13 @@ public enum AccoladeType {
 	public String getIcon() {
 		return icon;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public BigInteger getValue() {
+		return value;
+	}
+
 }
