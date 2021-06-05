5b74a413cf6cdd82cc50d23ba00598bf47ca6fa0
Gartham <Gartham@dusttoash.org>
2021-06-05 16:36:59 -0500

    Update CompoundMathExpression.java

--- src/gartham/c10ver/games/math/building/CompoundMathExpression.java
+++ src/gartham/c10ver/games/math/building/CompoundMathExpression.java
@@ -9,18 +9,25 @@ import gartham.c10ver.games.math.MathProblem;
 public class CompoundMathExpression implements MathProblem, MathExpression {
 
 	public enum Operator {
-		ADD(1, "+", BigDecimal::add), SUBTRACT(1, "-", BigDecimal::subtract), MULTIPLY(2, "*", BigDecimal::multiply),
-		DIVIDE(2, "/", (a, b) -> a.divide(b, RoundingMode.HALF_UP));
+		ADD(1, "+", BigDecimal::add), SUBTRACT(1, "-", BigDecimal::subtract, true),
+		MULTIPLY(2, "*", BigDecimal::multiply), DIVIDE(2, "/", (a, b) -> a.divide(b, RoundingMode.HALF_UP), true);
 
 		private final int ord;
 		private final String chr;
 		private final BiFunction<? super BigDecimal, ? super BigDecimal, ? extends BigDecimal> op;
+		private final boolean wrapRight;
 
 		private Operator(int ord, String chr,
 				BiFunction<? super BigDecimal, ? super BigDecimal, ? extends BigDecimal> op) {
+			this(ord, chr, op, false);
+		}
+
+		private Operator(int ord, String chr,
+				BiFunction<? super BigDecimal, ? super BigDecimal, ? extends BigDecimal> op, boolean wrap) {
 			this.ord = ord;
 			this.chr = chr;
 			this.op = op;
+			wrapRight = wrap;
 		}
 
 		public int ord() {
@@ -40,36 +47,44 @@ public class CompoundMathExpression implements MathProblem, MathExpression {
 	private final int lo, ro;
 	private final BigDecimal res;
 
+	/**
+	 * Wraps the provided {@link MathExpression} in parentheses. This makes its
+	 * behavior the same as a {@link Term} (in ordinality) and makes its
+	 * {@link MathExpression#problem()} have parentheses around it.
+	 * 
+	 * @param other The {@link MathExpression} to wrap.
+	 * @return A {@link MathExpression} that is equivalent to the provided one, but
+	 *         wrapped in parentheses.
+	 */
+	public static MathExpression wrap(MathExpression other) {
+		return new MathExpression() {
+
+			@Override
+			public String problem() {
+				return '(' + other.problem() + ')';
+			}
+
+			@Override
+			public BigDecimal eval() {
+				return other.eval();
+			}
+		};
+	}
+
 	public CompoundMathExpression(MathExpression first, Operator operator, MathExpression second) {
 		StringBuilder sb = new StringBuilder();
 
-		if (first.ro() >= 0) {
-			if (first.ro() < operator.ord()) {
-				sb.append('(').append(first.problem()).append(") ");
-				lo = operator.ord();
-			} else {
-				sb.append(first.problem()).append(' ');
-				lo = first.lo();
-			}
-		} else {
-			sb.append(first.problem()).append(' ');
-			lo = operator.ord();
-		}
+		if (first.ro() > 0 && first.lo() < operator.ord)
+			first = wrap(first);
+		if (second.ro() > 0 && (second.lo() < operator.ord || operator.wrapRight))
+			second = wrap(second);
 
-		sb.append(operator.chr()).append(' ');
+		ro = first.ro() > operator.ord ? first.ro() > second.ro() ? first.ro() : second.ro()
+				: operator.ord > second.ro() ? operator.ord : second.ro();
+		lo = first.lo() < operator.ord ? first.lo() < second.lo() ? first.lo() : second.lo()
+				: operator.ord < second.lo() ? operator.ord : second.lo();
 
-		if (second.lo() >= 0) {
-			if (second.lo() < operator.ord()) {
-				sb.append('(').append(second.problem()).append(')');
-				ro = operator.ord();
-			} else {
-				sb.append(second.problem());
-				ro = second.ro();
-			}
-		} else {
-			sb.append(second.problem());
-			ro = operator.ord();
-		}
+		sb.append(first.problem()).append(' ').append(operator.chr()).append(' ').append(second.problem());
 
 		val = sb.toString();
 		res = operator.operate(first.eval(), second.eval());
