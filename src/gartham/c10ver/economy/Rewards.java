2cc44898853cb8a9ffdd8c838bb0d49e11456043
Gartham <Gartham@dusttoash.org>
2021-06-07 03:20:35 -0500

    Update Rewards.java

--- src/gartham/c10ver/economy/Rewards.java
+++ src/gartham/c10ver/economy/Rewards.java
@@ -4,29 +4,33 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import gartham.c10ver.economy.items.ItemBunch;
 
 public class Rewards {
-	private final Map<String, ItemBunch<?>> items;
+	private final List<ItemBunch<?>> items;
 	private final List<Multiplier> multipliers;
 	private final BigInteger cloves;
 
+	public boolean hasCloves() {
+		return cloves != null && cloves.compareTo(BigInteger.ZERO) != 0;
+	}
+
 	public Rewards(Iterable<ItemBunch<?>> items, BigInteger cloves, List<Multiplier> multipliers) {
-		this.items = items instanceof Collection<?> ? new HashMap<>(((Collection<?>) items).size()) : new HashMap<>();
+		this.items = items instanceof Collection<?> ? new ArrayList<>(((Collection<?>) items).size())
+				: new ArrayList<>();
 		for (ItemBunch<?> ib : items)
-			this.items.put(ib.getItem().getItemType(), ib);
+			this.items.add(ib);
 		this.multipliers = multipliers;
 		this.cloves = cloves;
 	}
 
 	public Rewards(Iterable<ItemBunch<?>> items, BigInteger cloves, Multiplier... multipliers) {
-		this.items = items instanceof Collection<?> ? new HashMap<>(((Collection<?>) items).size()) : new HashMap<>();
+		this.items = items instanceof Collection<?> ? new ArrayList<>(((Collection<?>) items).size())
+				: new ArrayList<>();
 		for (ItemBunch<?> ib : items)
-			this.items.put(ib.getItem().getItemType(), ib);
+			this.items.add(ib);
 		this.multipliers = new ArrayList<>(multipliers.length);
 		for (Multiplier m : multipliers)
 			this.multipliers.add(m);
@@ -34,69 +38,54 @@ public class Rewards {
 	}
 
 	public Rewards(Iterable<ItemBunch<?>> items, BigInteger cloves) {
-		this.items = items instanceof Collection<?> ? new HashMap<>(((Collection<?>) items).size()) : new HashMap<>();
+		this.items = items instanceof Collection<?> ? new ArrayList<>(((Collection<?>) items).size())
+				: new ArrayList<>();
 		for (ItemBunch<?> ib : items)
-			this.items.put(ib.getItem().getItemType(), ib);
-		this.multipliers = Collections.emptyList();
-		this.cloves = cloves;
-	}
-
-	public Rewards(Map<String, ItemBunch<?>> items, BigInteger cloves) {
-		this.items = items;
+			this.items.add(ib);
 		this.multipliers = Collections.emptyList();
 		this.cloves = cloves;
 	}
 
 	public Rewards(ItemBunch<?>[] items, BigInteger cloves) {
-		this.items = new HashMap<>(items.length);
+		this.items = new ArrayList<>(items.length);
 		for (ItemBunch<?> ib : items)
-			this.items.put(ib.getItem().getItemType(), ib);
+			this.items.add(ib);
 		this.multipliers = Collections.emptyList();
 		this.cloves = cloves;
 	}
 
 	public Rewards(ItemBunch<?>[] items, BigInteger cloves, List<Multiplier> multipliers) {
-		this.items = new HashMap<>(items.length);
+		this.items = new ArrayList<>(items.length);
 		for (ItemBunch<?> ib : items)
-			this.items.put(ib.getItem().getItemType(), ib);
+			this.items.add(ib);
 		this.multipliers = multipliers;
 		this.cloves = cloves;
 	}
 
 	public Rewards(ItemBunch<?>[] items, BigInteger cloves, Multiplier... multipliers) {
-		this.items = new HashMap<>(items.length);
+		this.items = new ArrayList<>(items.length);
 		for (ItemBunch<?> ib : items)
-			this.items.put(ib.getItem().getItemType(), ib);
+			this.items.add(ib);
 		this.multipliers = new ArrayList<>(multipliers.length);
 		for (Multiplier m : multipliers)
 			this.multipliers.add(m);
 		this.cloves = cloves;
 	}
 
-	public Rewards(Map<String, ItemBunch<?>> items, BigInteger cloves, List<Multiplier> multipliers) {
-		this.items = items;
-		this.multipliers = multipliers;
-		this.cloves = cloves;
+	public boolean hasItems() {
+		return !items.isEmpty();
 	}
 
-	public Rewards(Map<String, ItemBunch<?>> items, BigInteger cloves, Multiplier... multipliers) {
-		this.items = items;
-		this.multipliers = new ArrayList<>(multipliers.length);
-		for (Multiplier m : multipliers)
-			this.multipliers.add(m);
-		this.cloves = cloves;
-	}
-
-	public Map<String, ItemBunch<?>> getItems() {
-		return Collections.unmodifiableMap(items);
-	}
-
-	public List<ItemBunch<?>> getItemList() {
-		return new ArrayList<>(items.values());
+	public List<ItemBunch<?>> getItemsAsList() {
+		return Collections.unmodifiableList(items);
 	}
 
 	public List<Multiplier> getMultipliers() {
 		return Collections.unmodifiableList(multipliers);
+	}
+
+	public boolean hasMultipliers() {
+		return !multipliers.isEmpty();
 	}
 
 	public BigInteger getCloves() {
