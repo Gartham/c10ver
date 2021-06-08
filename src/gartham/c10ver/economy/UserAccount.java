17f6ae0bc1e769a5ef77b8c2c4da2b5f7b1e737f
Gartham <Gartham@dusttoash.org>
2021-06-08 02:27:41 -0500

    Update UserAccount.java

--- src/gartham/c10ver/economy/UserAccount.java
+++ src/gartham/c10ver/economy/UserAccount.java
@@ -1,8 +1,23 @@
 package gartham.c10ver.economy;
 
 import java.io.File;
+import java.math.BigInteger;
 
 public class UserAccount extends Account implements Owned<User> {
+	private final Property<BigInteger> totalEarnings = bigIntegerProperty("total-earnings", BigInteger.ZERO);
+
+	public void setTotalEarnings(BigInteger earnings) {
+		totalEarnings.set(earnings);
+	}
+
+	public BigInteger getTotalEarnings() {
+		return totalEarnings.get();
+	}
+
+	public BigInteger addTotalEarnings(BigInteger earnings) {
+		return totalEarnings.set(totalEarnings.get().add(earnings)).get();
+	}
+
 	private final User user;
 
 	public User getUser() {
