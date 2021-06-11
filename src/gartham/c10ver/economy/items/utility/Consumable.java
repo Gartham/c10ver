e6de2ebbda45974f45549509bfff67b9649bb428
Gartham <Gartham@dusttoash.org>
2021-06-11 03:24:50 -0500

    Update Consumable.java

--- src/gartham/c10ver/economy/items/utility/Consumable.java
+++ src/gartham/c10ver/economy/items/utility/Consumable.java
@@ -1,12 +1,24 @@
 package gartham.c10ver.economy.items.utility;
 
-import gartham.c10ver.economy.User;
+import gartham.c10ver.economy.Rewards;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public interface Consumable {
 	/**
-	 * Consumes this {@link Consumable} as the provided {@link User}.
+	 * Calculates and returns the rewards from consuming this item.
 	 * 
-	 * @param user The user that will consume this item.
+	 * @param event The event that the item was consumed in.
+	 * @return The {@link Rewards}.
 	 */
-	void consume(User user);
+	Rewards consume(MessageReceivedEvent event);
+
+	/**
+	 * Returns the message to be displayed when this item is consumed. This is
+	 * typically something similar to <code>"Steve#0314 is opening a crate!"</code>
+	 * or <code>"Joe#8484 just ate 12 pizzas!</code>.
+	 * 
+	 * @param event The event that the item was consumed in.
+	 * @return The message.
+	 */
+	String message(MessageReceivedEvent event);
 }
