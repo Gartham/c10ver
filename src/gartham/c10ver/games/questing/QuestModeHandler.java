a4783ec63c16689baf7958999c7e55f128d83c48
Gartham <Gartham@dusttoash.org>
2021-06-16 02:21:08 -0500

    Create QuestModeHandler.java

---
+++ src/gartham/c10ver/games/questing/QuestModeHandler.java
@@ -0,0 +1,13 @@
+package gartham.c10ver.games.questing;
+
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+
+public class QuestModeHandler implements EventListener {
+
+	@Override
+	public void onEvent(GenericEvent event) {
+		
+	}
+
+}
