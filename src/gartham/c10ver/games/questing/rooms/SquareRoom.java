984af71d9d526818e96c43f7f2894adc13510d0a
Gartham <Gartham@dusttoash.org>
2021-06-17 03:10:39 -0500

    Create SquareRoom.java

---
+++ src/gartham/c10ver/games/questing/rooms/SquareRoom.java
@@ -0,0 +1,17 @@
+package gartham.c10ver.games.questing.rooms;
+
+public class SquareRoom implements Room {
+
+	private final char[][] map;
+
+	public SquareRoom(int size) {
+		this.map = new char[size][size];
+	}
+
+	@Override
+	public char[][] layout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
