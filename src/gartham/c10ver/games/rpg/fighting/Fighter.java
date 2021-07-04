0b14ad39f357af2242d1353d474e29b1b042640c
Gartham <Gartham@dusttoash.org>
2021-07-04 01:59:25 -0500

    Create Fighter.java

---
+++ src/gartham/c10ver/games/rpg/fighting/Fighter.java
@@ -0,0 +1,10 @@
+package gartham.c10ver.games.rpg.fighting;
+
+public interface Fighter extends Comparable<Fighter> {
+	int speed();
+
+	@Override
+	default int compareTo(Fighter o) {
+		return speed() - o.speed();
+	}
+}
