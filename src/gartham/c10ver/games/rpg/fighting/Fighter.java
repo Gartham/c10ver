4cd184d68144be370ffdec761c99e136f61aeb38
Gartham <Gartham@dusttoash.org>
2021-07-05 21:29:09 -0500

    Update Fighter.java

--- src/gartham/c10ver/games/rpg/fighting/Fighter.java
+++ src/gartham/c10ver/games/rpg/fighting/Fighter.java
@@ -3,6 +3,8 @@ package gartham.c10ver.games.rpg.fighting;
 public interface Fighter extends Comparable<Fighter> {
 	int speed();
 
+	int health();
+
 	@Override
 	default int compareTo(Fighter o) {
 		return speed() - o.speed();
