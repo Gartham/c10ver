27e7228655b078119c0b180294b9f3c53262a6e8
Gartham <Gartham@dusttoash.org>
2021-07-08 12:06:54 -0500

    Add factory functions to Action class

--- src/gartham/c10ver/actions/Action.java
+++ src/gartham/c10ver/actions/Action.java
@@ -3,5 +3,23 @@ package gartham.c10ver.actions;
 import java.util.function.Consumer;
 
 public interface Action extends Consumer<ActionInvocation> {
-	
+	public static Action msg(ActionMessage msg) {
+		return t -> msg.send(t.getClover(), t.getEvent().getChannel());
+	}
+
+	/**
+	 * Returns a new {@link Action} that executes the provided {@link Consumer}
+	 * action and then sends the provided {@link ActionMessage}.
+	 * 
+	 * @param msg    The {@link ActionMessage} to send once this {@link Action} is
+	 *               executed.
+	 * @param action The code to run when this {@link Action} is executed.
+	 * @return A new {@link Action} wrapping the provided objects.
+	 */
+	public static Action msg(ActionMessage msg, Consumer<ActionInvocation> action) {
+		return t -> {
+			action.accept(t);
+			msg.send(t.getClover(), t.getEvent().getChannel());
+		};
+	}
 }
