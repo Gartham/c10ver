17ab3eca56fb42bc02952ba2e6b032efcebd1ef6
Gartham <Gartham@dusttoash.org>
2021-07-11 11:38:00 -0500

    Update Team.java

--- src/gartham/c10ver/games/rpg/fighting/Team.java
+++ src/gartham/c10ver/games/rpg/fighting/Team.java
@@ -1,16 +1,19 @@
 package gartham.c10ver.games.rpg.fighting;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Set;
 
 import org.alixia.javalibrary.JavaTools;
 
 public class Team implements Iterable<Fighter> {
-	private final List<Fighter> members;
+	private final Set<Fighter> members;
 
-	public Team(List<Fighter> members) {
-		this.members = members;
+	public Team(Collection<? extends Fighter> members) {
+		this.members = new HashSet<>(members);
 	}
 
 	/**
@@ -19,13 +22,13 @@ public class Team implements Iterable<Fighter> {
 	 * @return {@link Collections#unmodifiableList(List)} of the members of this
 	 *         {@link Team}.
 	 */
-	public List<Fighter> viewMembers() {
-		return Collections.unmodifiableList(members);
+	public Set<Fighter> viewMembers() {
+		return Collections.unmodifiableSet(members);
 	}
 
 	@Override
-	public ListIterator<Fighter> iterator() {
-		return JavaTools.unmodifyingListIterator(members.listIterator());
+	public Iterator<Fighter> iterator() {
+		return JavaTools.unmodifyingIterator(members.iterator());
 	}
 
 }
