0703400edc1accacd146259c5aca430d2407d354
Gartham <Gartham@dusttoash.org>
2021-07-11 07:54:07 -0500

    Update CommandProcessor.java

--- src/gartham/c10ver/commands/CommandProcessor.java
+++ src/gartham/c10ver/commands/CommandProcessor.java
@@ -6,12 +6,13 @@ import java.util.List;
 public class CommandProcessor {
 	private List<Command> commands = new ArrayList<>();
 
-	public void run(CommandInvocation inv) {
+	public boolean run(CommandInvocation inv) {
 		for (Command c : commands)
 			if (c.match(inv)) {
 				c.exec(inv);
-				return;
+				return true;
 			}
+		return false;
 	}
 
 	public void register(Command comm) {
