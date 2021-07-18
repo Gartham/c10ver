85c79c47b8c3efc3785925078bf21abeb00e1089
Gartham <Gartham@dusttoash.org>
2021-07-18 18:27:49 -0500

    Create SimpleCreature.java

---
+++ src/gartham/c10ver/games/rpg/creatures/SimpleCreature.java
@@ -0,0 +1,41 @@
+package gartham.c10ver.games.rpg.creatures;
+
+import java.math.BigInteger;
+
+/**
+ * @author Gartham
+ *
+ */
+public class SimpleCreature extends Creature {
+
+	/**
+	 * Factors representing how intrinsically strong this type of creature is in the
+	 * respective stats.
+	 */
+	private final double hpf, attackf, speedf, deff;
+
+	@Override
+	public BigInteger getHp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigInteger getAttack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigInteger getSpeed() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BigInteger getDefense() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
