dd76e6015c8fbfb6a4856e79a90f6ffc804a1cdd
Gartham <Gartham@dusttoash.org>
2021-07-22 04:17:06 -0500

    Update FighterController.java

--- src/gartham/c10ver/games/rpg/fighting/FighterController.java
+++ src/gartham/c10ver/games/rpg/fighting/FighterController.java
@@ -1,5 +1,11 @@
 package gartham.c10ver.games.rpg.fighting;
 
 public interface FighterController {
-
+	/**
+	 * Controlls the {@link Fighter} linked to this {@link FighterController} for
+	 * its turn, and returns the number of ticks that the action taken takes.
+	 * 
+	 * @return The number of ticks taken by the action.
+	 */
+	int act();
 }
