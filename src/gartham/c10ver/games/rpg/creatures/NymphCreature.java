837cc9c94d544428b2336fe17cc76a650830c231
Gartham <Gartham@dusttoash.org>
2021-07-21 22:12:12 -0500

    Update NymphCreature to implement name attirbute

--- src/gartham/c10ver/games/rpg/creatures/NymphCreature.java
+++ src/gartham/c10ver/games/rpg/creatures/NymphCreature.java
@@ -4,14 +4,14 @@ import org.alixia.javalibrary.json.JSONObject;
 
 public class NymphCreature extends SimpleCreature {
 
-	public static final String TYPE = "nymph";
+	public static final String TYPE = "nymph", NAME = "Nymph";
 	private static final double HPF = .8, ATTACKF = 1.1, SPEEDF = .7, DEFF = .9;
 
 	public NymphCreature() {
 		super(TYPE,
 				"https://media.discordapp.net/attachments/807401695688261639/862522787319382046/nymph.png?width=632&height=676",
 				"https://media.discordapp.net/attachments/807401695688261639/862522787519528960/nymph_headshot.png",
-				"<:nymph_emoji:854622804514832384>", HPF, ATTACKF, SPEEDF, DEFF);
+				"<:nymph_emoji:854622804514832384>", "Nymph", HPF, ATTACKF, SPEEDF, DEFF);
 	}
 
 	public NymphCreature(JSONObject data) {
