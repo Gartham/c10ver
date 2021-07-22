27cf788e05ab91a46db58fdbb613fd3abae90e6b
Gartham <Gartham@dusttoash.org>
2021-07-22 13:42:34 -0500

    Update AttackAction.java

--- src/gartham/c10ver/games/rpg/fighting/battles/AttackAction.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/AttackAction.java
@@ -8,6 +8,14 @@ import gartham.c10ver.actions.ActionInvocation;
 public class AttackAction extends Action {
 	private final String description;
 
+	public static AttackAction msg(String emoji, String name, String desc, AttackActionMessage msg,
+			Consumer<ActionInvocation> action) {
+		return new AttackAction(emoji, name, t -> {
+			action.accept(t);
+			msg.send(t.getClover(), t.getEvent().getChannel(), t.getEvent().getUser());
+		}, desc);
+	}
+
 	public AttackAction(String emoji, String name, Consumer<ActionInvocation> action, String description) {
 		super(emoji, name, action);
 		this.description = description;
