ec1d1fef13ad1d2818d611aa40feb85b6208e279
Gartham <Gartham@dusttoash.org>
2021-07-21 22:12:22 -0500

    Update stuff with battles

--- src/gartham/c10ver/games/rpg/fighting/battles/Battle.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/Battle.java
@@ -1,8 +1,10 @@
 package gartham.c10ver.games.rpg.fighting.battles;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,10 +24,29 @@ public class Battle {
 	private final List<Fighter> battleQueue = new ArrayList<>();
 	private final List<Team> teams;
 
+	private void debug(String msg) {
+		channel.sendMessage("[DEBUG]: " + msg).queue();
+	}
+
 	public void start() {
-		channel.sendMessage(new EmbedBuilder().setTitle(
-				String.join(" vs ", JavaTools.mask(teams, t -> '`' + Utilities.stripBackticks(t.getName()) + '`')))
-				.build()).queue();
+
+		debug("Speeds: ["
+				+ String.join(", ", JavaTools.mask(battleQueue, a -> a.getName() + "(`" + a.getSpeed() + "`)")) + ']');
+
+		// Assign initial ticks.
+		var max = battleQueue.get(0).getSpeed();
+
+		for (Fighter f : battleQueue)
+			setTTT(f, new BigDecimal(max.subtract(f.getSpeed())).multiply(BigDecimal.valueOf(Math.random() / 5 + 0.9))
+					.intValue());
+
+		Collections.sort(battleQueue, (o1, o2) -> Integer.compare(getTTT(o1), getTTT(o2)));
+
+		EmbedBuilder builder = new EmbedBuilder().setTitle(
+				String.join(" vs ", JavaTools.mask(teams, t -> '`' + Utilities.stripBackticks(t.getName()) + '`')));
+		for (var f : battleQueue)
+			builder.addField(f.getEmoji() + ' ' + f.getTeam().getName(), "\uD83D\uDD50\uFE0F " + getTTT(f), false);
+		channel.sendMessage(builder.build()).queue();
 	}
 
 	private int getTTT(Fighter fighter) {
@@ -42,7 +63,9 @@ public class Battle {
 		for (var t : teams) {
 			this.teams.add(t);
 			for (var f : t)
-				battleQueue.add(-Collections.binarySearch(battleQueue, f) - 1, f);
+				battleQueue.add(
+						-Collections.binarySearch(battleQueue, f, Comparator.<Fighter>naturalOrder().reversed()) - 1,
+						f);
 		}
 	}
 
