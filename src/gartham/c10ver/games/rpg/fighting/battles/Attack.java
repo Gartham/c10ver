ef45b90f8f330909340ab1efeae900cd83100b2a
Gartham <Gartham@dusttoash.org>
2021-07-25 04:54:55 -0500

    Create Attack.java

---
+++ src/gartham/c10ver/games/rpg/fighting/battles/Attack.java
@@ -0,0 +1,29 @@
+package gartham.c10ver.games.rpg.fighting.battles;
+
+import java.math.BigInteger;
+
+public class Attack {
+	private final BigInteger damage, defDrain, attackDrain;
+
+	public Attack(BigInteger damage, BigInteger defDrain, BigInteger attackDrain) {
+		this.damage = damage;
+		this.defDrain = defDrain;
+		this.attackDrain = attackDrain;
+	}
+
+	public Attack(BigInteger damage) {
+		this(damage, BigInteger.ZERO, BigInteger.ZERO);
+	}
+
+	public BigInteger getDamage() {
+		return damage;
+	}
+
+	public BigInteger getDefDrain() {
+		return defDrain;
+	}
+
+	public BigInteger getAttackDrain() {
+		return attackDrain;
+	}
+}
