4540c1d4df54bb67007d26ec71fc42731c12c18a
Gartham <Gartham@dusttoash.org>
2021-07-25 04:55:05 -0500

    Create BasicBattle.java

---
+++ src/gartham/c10ver/games/rpg/fighting/battles/BasicBattle.java
@@ -0,0 +1,13 @@
+package gartham.c10ver.games.rpg.fighting.battles;
+
+import gartham.c10ver.games.rpg.fighting.fighters.Fighter;
+
+public class BasicBattle extends Battle<BattleAction> {
+
+	@Override
+	protected int handleAction(BattleAction action, Fighter fighter) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
