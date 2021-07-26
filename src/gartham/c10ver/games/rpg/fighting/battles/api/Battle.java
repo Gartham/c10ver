5130eaa81f8c73a9945d31df844d31003a0c4910
Gartham <Gartham@dusttoash.org>
2021-07-26 07:00:30 -0500

    Update Battles to use Fighter type instead of SimpleFighter

--- src/gartham/c10ver/games/rpg/fighting/battles/api/Battle.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/api/Battle.java
@@ -10,46 +10,45 @@ import java.util.List;
 import java.util.Map;
 
 import gartham.c10ver.games.rpg.fighting.fighters.Fighter;
-import gartham.c10ver.games.rpg.fighting.fighters.SimpleFighter;
 
 /**
  * <p>
  * A {@link Battle} is a fight between at least two {@link Team}s, each composed
- * of at least one {@link SimpleFighter}. {@link Battle}s are <b>turn-based</b>, with
+ * of at least one {@link Fighter}. {@link Battle}s are <b>turn-based</b>, with
  * each turn being taken through the doing of an <b>action</b> by a
- * {@link SimpleFighter}. Each action "takes" a certain number of "ticks", which are,
+ * {@link Fighter}. Each action "takes" a certain number of "ticks", which are,
  * in a sense, time in a battle. When a fighter acts during its turn, the number
  * of ticks the action takes is the number of ticks "until" it is that same
- * {@link SimpleFighter}'s turn to make its next move. Upon the conclusion of any
- * turn, if all {@link SimpleFighter}s on a {@link Team} are considered dead (i.e.
+ * {@link Fighter}'s turn to make its next move. Upon the conclusion of any
+ * turn, if all {@link Fighter}s on a {@link Team} are considered dead (i.e.
  * their health is <code>0</code>), that {@link Team} is considered dead. Once
  * all but one {@link Team} is considered dead, that {@link Team} wins the
  * {@link Battle}. If, upon the conclusion of a turn, all {@link Team}s are
  * dead, the battle is a considered to have been won by the {@link Team} whose
- * {@link SimpleFighter} had the last turn.
+ * {@link Fighter} had the last turn.
  * </p>
  * <p>
- * Each turn, a {@link SimpleFighter} takes an <b>action</b>. Actions take a certain
+ * Each turn, a {@link Fighter} takes an <b>action</b>. Actions take a certain
  * number of ticks and can modify the {@link Battle} in any way that the
  * {@link Battle} permits. Subclasses may expose behavior and functionality that
  * modifies the battle as they see fit. They may also expose behavior and
  * functionality that build upon the basic concept of a {@link Battle} (e.g.
- * adding "effects" that target {@link SimpleFighter}s, which are kept track of by the
+ * adding "effects" that target {@link Fighter}s, which are kept track of by the
  * {@link Battle} and cease their effects after a certain number of ticks,
- * affecting the targeted {@link SimpleFighter} while extant). {@link Battle}
- * subclasses should expose the actions that can be taken by a {@link SimpleFighter}
+ * affecting the targeted {@link Fighter} while extant). {@link Battle}
+ * subclasses should expose the actions that can be taken by a {@link Fighter}
  * to calling code through some means.
  * </p>
  * 
  * @author Gartham
  *
  * @param <A> The type representing the actions that can be taken by
- *            {@link SimpleFighter}s tracked by this {@link Battle}.
+ *            {@link Fighter}s tracked by this {@link Battle}.
  */
 public abstract class Battle<A> {
 
-	private final Map<SimpleFighter, Integer> ticksTillTurn = new HashMap<>();
-	private final List<SimpleFighter> battleQueue = new ArrayList<>();
+	private final Map<Fighter, Integer> ticksTillTurn = new HashMap<>();
+	private final List<Fighter> battleQueue = new ArrayList<>();
 	private final List<Team> teams;
 	private State state = State.UNSTARTED;
 
@@ -77,7 +76,7 @@ public abstract class Battle<A> {
 	/**
 	 * Starts this {@link Battle} by setting the {@link #state} to
 	 * {@link State#RUNNING} and assigning <b>initial ticks</b> to each
-	 * {@link SimpleFighter}.
+	 * {@link Fighter}.
 	 */
 	public void start() {
 		if (state != State.UNSTARTED)
@@ -91,30 +90,30 @@ public abstract class Battle<A> {
 
 	/**
 	 * <p>
-	 * This is the method that assigns <b>initial ticks</b> to each {@link SimpleFighter}.
+	 * This is the method that assigns <b>initial ticks</b> to each {@link Fighter}.
 	 * Upon a call to this method by {@link #start()}, the provided
-	 * <code>queue</code> of {@link SimpleFighter}s (1) contains all of the
-	 * {@link SimpleFighter}s participating in the {@link Battle} and (2) is sorted, in
-	 * descending order, by {@link SimpleFighter#getSpeed() speed}. This means that the
-	 * {@link SimpleFighter} with the highest speed is positioned at index <code>0</code>.
+	 * <code>queue</code> of {@link Fighter}s (1) contains all of the
+	 * {@link Fighter}s participating in the {@link Battle} and (2) is sorted, in
+	 * descending order, by {@link Fighter#getSpeed() speed}. This means that the
+	 * {@link Fighter} with the highest speed is positioned at index <code>0</code>.
 	 * </p>
 	 * <p>
 	 * This method is <b>only</b> tasked with setting the number of ticks for each
-	 * {@link SimpleFighter} through the {@link #setTicks(SimpleFighter, int)} method. The
+	 * {@link Fighter} through the {@link #setTicks(Fighter, int)} method. The
 	 * provided {@link List} is unmodifiable, as the {@link #battleQueue battle
 	 * queue} used by {@link Battle} is sorted according to ticks immediately after
 	 * this method is called by {@link #start()}.
 	 */
-	protected void assignInitialTicks(List<SimpleFighter> queue) {
+	protected void assignInitialTicks(List<Fighter> queue) {
 		// Assign initial ticks.
 		var max = battleQueue.get(0).getSpeed();
 
-		for (SimpleFighter f : queue)
+		for (Fighter f : queue)
 			ticksTillTurn.put(f, new BigDecimal(max.subtract(f.getSpeed()))
 					.multiply(BigDecimal.valueOf(Math.random() / 5 + 0.9)).intValue());
 	}
 
-	protected final void setTicks(SimpleFighter fighter, int ticks) {
+	protected final void setTicks(Fighter fighter, int ticks) {
 		ticksTillTurn.put(fighter, ticks);
 	}
 
@@ -123,7 +122,7 @@ public abstract class Battle<A> {
 	}
 
 	/**
-	 * Acts as the current {@link SimpleFighter}.
+	 * Acts as the current {@link Fighter}.
 	 * 
 	 * @param action The action to take.
 	 */
@@ -143,14 +142,14 @@ public abstract class Battle<A> {
 	/**
 	 * Performs any unique behavior specified by the provided <code>action</code>,
 	 * and returns the number of ticks that the action has taken. This method should
-	 * <b>not</b> add said number of ticks to the {@link SimpleFighter} that performed the
+	 * <b>not</b> add said number of ticks to the {@link Fighter} that performed the
 	 * action. That is handled by {@link #act(Object)} in the {@link Battle} class.
 	 * 
 	 * @param action  The action taken.
-	 * @param fighter The {@link SimpleFighter} that took the action (i.e. the current
+	 * @param fighter The {@link Fighter} that took the action (i.e. the current
 	 *                fighter). At the beginning of this method call, (as per normal
 	 *                {@link Battle} behavior), this argument should be exactly the
-	 *                same as the {@link SimpleFighter} in the front (position
+	 *                same as the {@link Fighter} in the front (position
 	 *                <code>0</code>) of the {@link #battleQueue battle queue}.
 	 * @return The number of ticks that the action has taken.
 	 */
@@ -158,7 +157,7 @@ public abstract class Battle<A> {
 
 	/**
 	 * Sorts the battle queue according to ticks. This is automatically done at the
-	 * conclusion of every turn, specifically after the ticks to the {@link SimpleFighter}
+	 * conclusion of every turn, specifically after the ticks to the {@link Fighter}
 	 * that has just performed its action have been applied to it.
 	 */
 	protected final void sortQueue() {
