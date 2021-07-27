620861eb78634e23a17183c2d91d615b1f3ebce3
Gartham <Gartham@dusttoash.org>
2021-07-27 03:46:29 -0500

    Update GarmonBattle.java

--- src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonBattle.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonBattle.java
@@ -1,9 +1,20 @@
 package gartham.c10ver.games.rpg.fighting.battles.app;
 
+import java.util.Collection;
+
 import gartham.c10ver.games.rpg.fighting.battles.api.Battle;
 import gartham.c10ver.games.rpg.fighting.battles.api.Team;
 
 public class GarmonBattle extends Battle<GarmonBattleAction, GarmonFighter, Team<GarmonFighter>> {
+
+	public GarmonBattle(Collection<Team<GarmonFighter>> teams) {
+		super(teams);
+	}
+
+	@SafeVarargs
+	public GarmonBattle(Team<GarmonFighter>... teams) {
+		super(teams);
+	}
 
 	@Override
 	protected int handleAction(GarmonBattleAction action, GarmonFighter fighter) {
@@ -19,7 +30,7 @@ public class GarmonBattle extends Battle<GarmonBattleAction, GarmonFighter, Team
 			var ticks = getTicks(getNextNthOpponent(0, fighter));
 			setTicks(fighter, ticks);
 			break;
-			
+
 		case SPECIAL_ATTACK:
 			var attack = action.getSpecialAttack();
 			attack.getTarget().damage(attack.getDamage());
