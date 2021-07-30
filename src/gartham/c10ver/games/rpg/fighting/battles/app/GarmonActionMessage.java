90b7d4a96bf687a6d4862dc4f5774eae823ba2fb
Gartham <Gartham@dusttoash.org>
2021-07-30 03:43:51 -0500

    Update GarmonActionMessage.java

--- src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonActionMessage.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonActionMessage.java
@@ -1,27 +1,27 @@
 package gartham.c10ver.games.rpg.fighting.battles.app;
 
-import gartham.c10ver.actions.ActionMessage;
 import gartham.c10ver.actions.DetailedAction;
 import gartham.c10ver.actions.DetailedActionMessage;
 import gartham.c10ver.games.rpg.RPGUtils;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 
 public class GarmonActionMessage extends DetailedActionMessage<DetailedAction> {
+	private final GarmonFighter creature;
 
 	public GarmonActionMessage(GarmonFighter creature, DetailedAction... actions) {
 		super(actions);
+		this.creature = creature;
+	}
+
+	@Override
+	protected void buildEmbed(EmbedBuilder eb) {
+		super.buildEmbed(eb);
 		eb.setTitle(creature.getName() + "'s Turn").setThumbnail(creature.getHeadshot());
 		eb.setDescription("**" + creature.getName() + "**\nHealth: " + creature.getHealthString()
 				+ RPGUtils.calcHealthbar(creature.getHealth(), creature.getMaxHealth()));
 
-		for (var ga : actions)
-			eb.addField(ga.getEmoji() + ' ' + ga.getName(), ga.getDescription(), true);
-	}
-
-	@Override
-	public MessageEmbed embed() {
-		return eb.build();
+		for (var ga : getActions())
+			eb.addField(ga.getEmoji() + ' ' + ga.getName(), ga.getDetails(), true);
 	}
 
 }
