a0b6f119ddb220a74c4e2e357de5b002b71d0739
Gartham <gartham@dusttoash.org>
2021-09-19 04:41:21 -0500

    Reduce daily crate rewards

--- src/gartham/c10ver/economy/items/utility/crates/DailyCrate.java
+++ src/gartham/c10ver/economy/items/utility/crates/DailyCrate.java
@@ -1,14 +1,11 @@
 package gartham.c10ver.economy.items.utility.crates;
 
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.alixia.javalibrary.json.JSONObject;
 
-import gartham.c10ver.economy.AbstractMultiplier;
-import gartham.c10ver.economy.Multiplier;
 import gartham.c10ver.economy.Rewards;
 import gartham.c10ver.economy.items.ItemBunch;
 import gartham.c10ver.economy.items.utility.foodstuffs.Sandwich;
@@ -35,14 +32,12 @@ public class DailyCrate extends LootCrateItem {
 		List<ItemBunch<?>> items = new ArrayList<>();
 		if (Math.random() < 0.05)
 			items.add(new ItemBunch<>(new Sandwich(), Math.random() < 0.2 ? BigInteger.TWO : BigInteger.ONE));
-		if (Math.random() < 0.1)
-			items.add(new ItemBunch<>(new DailyCrate(), BigInteger.valueOf(3)));
-		else if (Math.random() < 0.04)
-			items.add(new ItemBunch<>(new WeeklyCrate(), BigInteger.ONE));
-		BigInteger cloves = BigInteger.valueOf((long) (Math.random() * 25 + 5));
-		return Math.random() < 0.01
-				? new Rewards(items, cloves, AbstractMultiplier.ofSec(90, BigDecimal.valueOf(25, 2)))
-				: new Rewards(items, cloves);
+		if (Math.random() < 0.3)
+			items.add(new ItemBunch<>(new DailyCrate()));
+		else if (Math.random() < 0.007)
+			items.add(new ItemBunch<>(new WeeklyCrate()));
+		BigInteger cloves = BigInteger.valueOf((long) (Math.random() * 250 + 100));
+		return new Rewards(items, cloves);
 	}
 
 }
