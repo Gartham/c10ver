7a51fdd21c64e89d4e210ca62bb9e2d2f4755487
Gartham <gartham@dusttoash.org>
2021-10-14 21:09:19 -0500

    Update ActionButton.java

--- src/gartham/c10ver/response/actions/ActionButton.java
+++ src/gartham/c10ver/response/actions/ActionButton.java
@@ -1,5 +1,36 @@
 package gartham.c10ver.response.actions;
 
-public class ActionButton {
+import java.util.function.Consumer;
+
+import net.dv8tion.jda.api.interactions.components.Component;
+
+public class ActionButton extends Action {
+	private Component component;
+	private String emoji;
+
+	public String getEmoji() {
+		return emoji;
+	}
+
+	public void setComponent(Component component) {
+		this.component = component;
+	}
+
+	public Component getComponent() {
+		return component;
+	}
+
+	public ActionButton(Component component) {
+		this.component = component;
+	}
+
+	public ActionButton(Consumer<ActionInvocation> action, Component component) {
+		super(action);
+		this.component = component;
+	}
+
+	public void setEmoji(String emoji) {
+		this.emoji = emoji;
+	}
 
 }
