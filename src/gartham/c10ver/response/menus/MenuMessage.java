126d38f421bbebdec5b0eacebbb32106043205a8
Gartham <gartham@dusttoash.org>
2021-10-14 20:48:11 -0500

    Update MenuMessage.java

--- src/gartham/c10ver/response/menus/MenuMessage.java
+++ src/gartham/c10ver/response/menus/MenuMessage.java
@@ -6,6 +6,7 @@ import java.util.List;
 import gartham.c10ver.Clover;
 import gartham.c10ver.response.actions.ActionReaction;
 import gartham.c10ver.response.actions.Action;
+import gartham.c10ver.response.actions.ActionButton;
 import gartham.c10ver.response.actions.ActionMessage;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageChannel;
@@ -17,45 +18,36 @@ import net.dv8tion.jda.api.entities.User;
  * that have emojis or buttons at the bottom which do something when clicked)
  * through a consistent format. A menu message not only adds the emojis/buttons
  * to the message after/before it's sent, a {@link MenuMessage} will actually
- * <i>build the message</i>, based off of the {@link ActionReaction}s the message will
- * let the user select. Essentially this class lets you present the user with an
- * {@link ActionMessage} but this class will also build a menu in the message,
- * possibly with descriptions, named entries, etc.
+ * <i>build the message</i>, based off of the {@link ActionReaction}s the
+ * message will let the user select. Essentially this class lets you present the
+ * user with an {@link ActionMessage} but this class will also build a menu in
+ * the message, possibly with descriptions, named entries, etc.
  * 
  * @author Gartham
  *
- * @param <A> The type of {@link ActionReaction} (typically corresponds to the type of
- *            {@link MenuMessage}).
+ * @param <A> The type of {@link ActionReaction} (typically corresponds to the
+ *            type of {@link MenuMessage}).
  */
-public abstract class MenuMessage<A extends Action> {
+public abstract class MenuMessage<R extends ActionReaction, B extends ActionButton> {
 
 	protected abstract void buildEmbed(EmbedBuilder builder);
 
-	private final ActionMessage<A> am;
+	private final ActionMessage<R, B> am;
 
-	public ActionMessage<A> getAm() {
+	public ActionMessage<R, B> getAm() {
 		return am;
 	}
 
-	public final List<A> getActions() {
+	public List<B> getButtons() {
+		return am.getButtons();
+	}
+
+	public List<R> getReactions() {
 		return am.getReactions();
 	}
 
-	public MenuMessage(ActionMessage<A> am) {
+	public MenuMessage(ActionMessage<R, B> am) {
 		this.am = am;
-	}
-
-	@SafeVarargs
-	public MenuMessage(A... actions) {
-		this(new ActionMessage<>(actions));
-	}
-
-	public MenuMessage(Iterable<A> actions) {
-		this(new ActionMessage<>(actions));
-	}
-
-	public MenuMessage(Iterator<A> actions) {
-		this(new ActionMessage<>(actions));
 	}
 
 	public final MessageEmbed embed() {
