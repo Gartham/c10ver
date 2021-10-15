f54691aed602e583677cdc77a430b346d8f6882b
Gartham <gartham@dusttoash.org>
2021-10-14 21:08:35 -0500

    Create NamedActionButton.java

---
+++ src/gartham/c10ver/response/actions/NamedActionButton.java
@@ -0,0 +1,39 @@
+package gartham.c10ver.response.actions;
+
+import java.util.function.Consumer;
+
+import net.dv8tion.jda.api.interactions.components.Component;
+
+public class NamedActionButton extends ActionButton {
+
+	public NamedActionButton(Consumer<ActionInvocation> action, Component component, String name, String emoji) {
+		super(action, component);
+		this.name = name;
+		this.emoji = emoji;
+	}
+
+	public NamedActionButton(Component component, String name, String emoji) {
+		super(component);
+		this.name = name;
+		this.emoji = emoji;
+	}
+
+	private String name, emoji;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmoji() {
+		return emoji;
+	}
+
+	public void setEmoji(String emoji) {
+		this.emoji = emoji;
+	}
+
+}
