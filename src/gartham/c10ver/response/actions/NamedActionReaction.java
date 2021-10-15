43f53f8b938816da662d6b6b71431039b95376fe
Gartham <gartham@dusttoash.org>
2021-10-14 21:09:32 -0500

    Update NamedActionReaction.java

--- src/gartham/c10ver/response/actions/NamedActionReaction.java
+++ src/gartham/c10ver/response/actions/NamedActionReaction.java
@@ -1,5 +1,21 @@
 package gartham.c10ver.response.actions;
 
+import java.util.function.Consumer;
+
 public class NamedActionReaction extends ActionReaction {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public NamedActionReaction(String emoji, Consumer<ActionInvocation> action, String name) {
+		super(emoji, action);
+		this.name = name;
+	}
 
 }
