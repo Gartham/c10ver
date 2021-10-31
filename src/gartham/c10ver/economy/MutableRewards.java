cd209589f75c46a259961aaa1dca86e0ae724780
Gartham <gartham@dusttoash.org>
2021-10-30 23:15:41 -0500

    Create MutableRewards.java

---
+++ src/gartham/c10ver/economy/MutableRewards.java
@@ -0,0 +1,118 @@
+package gartham.c10ver.economy;
+
+import java.io.File;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.alixia.javalibrary.json.JSONArray;
+import org.alixia.javalibrary.json.JSONObject;
+
+import gartham.c10ver.economy.items.Inventory;
+import gartham.c10ver.economy.items.ItemBunch;
+import gartham.c10ver.utils.Utilities;
+
+public class MutableRewards extends Rewards {
+
+	@Override
+	public void setInventory(Inventory n) {
+		super.setInventory(n);
+	}
+
+	@Override
+	public Inventory getItemsModifiable() {
+		return super.getItemsModifiable();
+	}
+
+	@Override
+	public Map<AbstractMultiplier, Integer> getMultipliersModifiable() {
+		return super.getMultipliersModifiable();
+	}
+
+	public MutableRewards() {
+	}
+
+	public MutableRewards(AbstractMultiplier... items) {
+		super(items);
+	}
+
+	public MutableRewards(BigInteger cloves) {
+		super(cloves);
+	}
+
+	public MutableRewards(ItemBunch<?>... items) {
+		super(items);
+	}
+
+	public MutableRewards(ItemBunch<?>[] items, BigInteger cloves, AbstractMultiplier... multipliers) {
+		super(items, cloves, multipliers);
+	}
+
+	public MutableRewards(ItemBunch<?>[] items, BigInteger cloves, List<AbstractMultiplier> multipliers) {
+		super(items, cloves, multipliers);
+	}
+
+	public MutableRewards(ItemBunch<?>[] items, BigInteger cloves) {
+		super(items, cloves);
+	}
+
+	public MutableRewards(Iterable<ItemBunch<?>> items, BigInteger cloves, AbstractMultiplier... multipliers) {
+		super(items, cloves, multipliers);
+	}
+
+	public MutableRewards(Iterable<ItemBunch<?>> items, BigInteger cloves, Iterable<AbstractMultiplier> multipliers) {
+		super(items, cloves, multipliers);
+	}
+
+	public MutableRewards(Iterable<ItemBunch<?>> items, BigInteger cloves) {
+		super(items, cloves);
+	}
+
+	public MutableRewards(Iterable<ItemBunch<?>> items) {
+		super(items);
+	}
+
+	public MutableRewards(File dir) {
+		loadFrom(dir);
+	}
+
+	/**
+	 * Updates this {@link Rewards} to represent the {@link Rewards} saved into the
+	 * specified folder. This is the instance-method symmetric counterpart of
+	 * {@link #save(File)}.
+	 * 
+	 * @param rewardsFolder The folder that will be read from.
+	 */
+	public void loadFrom(File rewardsFolder) {
+		File i = new File(rewardsFolder, "items");
+		Inventory inv = new Inventory();
+		inv.load(i);
+
+		JSONObject rewards = Utilities.loadObj(new File(rewardsFolder, "rewards.txt"));
+		BigInteger cloves;
+		Map<AbstractMultiplier, Integer> multipliers;
+		if (rewards != null) {
+			cloves = new BigInteger(rewards.getString("c"));
+
+			JSONArray a = (JSONArray) rewards.get("m");
+			multipliers = new HashMap<>();
+			for (var o : a) {
+				JSONObject obj = (JSONObject) o;
+				AbstractMultiplier m = new AbstractMultiplier(new BigDecimal(obj.getString("a")),
+						Duration.parse(obj.getString("d")));
+				multipliers.put(m, obj.getInt("c"));
+			}
+		} else {
+			cloves = BigInteger.ZERO;
+			multipliers = new HashMap<>();
+		}
+
+		setInventory(inv);
+		setCloves(cloves);
+		setMultipliers(multipliers);
+	}
+
+}
