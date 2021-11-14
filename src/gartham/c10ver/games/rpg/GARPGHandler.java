cc6f5f4f793358f2d658c95dcba7f0d468da840f
Gartham <gartham@dusttoash.org>
2021-11-14 02:46:14 -0600

    Update GARPGHandler.java

--- src/gartham/c10ver/games/rpg/GARPGHandler.java
+++ src/gartham/c10ver/games/rpg/GARPGHandler.java
@@ -1,13 +1,16 @@
 package gartham.c10ver.games.rpg;
 
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
-public class QuestModeHandler implements EventListener {
+public class GARPGHandler implements EventListener {
 
 	@Override
 	public void onEvent(GenericEvent event) {
-		
+		if (event instanceof GuildMessageReceivedEvent) {
+			
+		}
 	}
 
 }
