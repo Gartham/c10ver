955d054a4e5b9f5431b99e61c8829f2ba6385fcf
Gartham <gartham@dusttoash.org>
2021-11-13 20:36:08 -0600

    Make GarmonBattleManager give more accurate output when a player is fighting

--- src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonBattleManager.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonBattleManager.java
@@ -21,17 +21,24 @@ public final class GarmonBattleManager {
 	private final GarmonTeam opponentTeam, playerTeam;
 	private final Clover clover;
 	private final User player;
+	private final GarmonFighter playerFighter;
 
 	private final TextChannel chan;
 
 	public GarmonBattleManager(GarmonBattle battle, GarmonTeam opponentTeam, GarmonTeam playerTeam, Clover clover,
-			User player, TextChannel chan) {
+			User player, TextChannel chan, GarmonFighter playerFighter) {
+		this.playerFighter = playerFighter;
 		this.battle = battle;
 		this.opponentTeam = opponentTeam;
 		this.playerTeam = playerTeam;
 		this.clover = clover;
 		this.player = player;
 		this.chan = chan;
+	}
+
+	public GarmonBattleManager(GarmonBattle battle, GarmonTeam opponentTeam, GarmonTeam playerTeam, Clover clover,
+			User player, TextChannel chan) {
+		this(battle, opponentTeam, playerTeam, clover, player, chan, null);
 	}
 
 	public void start() {
@@ -46,7 +53,7 @@ public final class GarmonBattleManager {
 	private void next() {
 		var actor = battle.getActingFighter();
 		if (playerTeam.contains(actor)) {
-			userTurnMessage().send(clover, chan, player);
+			userTurnMessage(actor).send(clover, chan, player);
 		} else {
 			var c = (Consumer<Object>) t -> {
 				List<GarmonFighter> list = battle.getRemainingFighters(playerTeam);
@@ -61,8 +68,9 @@ public final class GarmonBattleManager {
 		}
 	}
 
-	private GarmonActionMessage userTurnMessage() {
-		GarmonActionMessage m = new GarmonActionMessage(battle.getActingFighter(), surrender(), skipTurn());
+	private GarmonActionMessage userTurnMessage(GarmonFighter actor) {
+		GarmonActionMessage m = new GarmonActionMessage(battle.getActingFighter(), surrender(),
+				skipTurn(actor == playerFighter));
 		m.getReactions().add(1, attack(m));
 		m.getReactions().add(info(m));
 		return m;
@@ -78,11 +86,13 @@ public final class GarmonBattleManager {
 		});
 	}
 
-	private DetailedActionReaction skipTurn() {
-		return new DetailedActionReaction("\uD83D\uDCA8", "Skip Turn", "Pass up this creature's move.", t -> {
-			battle.act(new GarmonBattleAction(ActionType.SKIP_TURN));
-			next();
-		});
+	private DetailedActionReaction skipTurn(boolean player) {
+		return new DetailedActionReaction("\uD83D\uDCA8", "Skip Turn",
+				"Pass up " + (player ? "your" : "this creature's") + "move.", t -> {
+					battle.act(new GarmonBattleAction(ActionType.SKIP_TURN));
+					next();
+				});
+
 	}
 
 	private void sendAttackMessage(ActionCompletion<GarmonActionResult, GarmonFighter> act) {
@@ -109,8 +119,8 @@ public final class GarmonBattleManager {
 				var dam = new DetailedMenuMessage<>(new ActionMessage<>());
 				var oplist = battle.getRemainingFighters(opponentTeam);
 				for (var v : oplist)
-					dam.getReactions().add(
-							new DetailedActionReaction(v.getName(), "\uD83D\uDD50\uFE0F " + battle.getFighterTicks(v), t1 -> {
+					dam.getReactions().add(new DetailedActionReaction(v.getName(),
+							"\uD83D\uDD50\uFE0F " + battle.getFighterTicks(v), t1 -> {
 								var act = battle.act(new GarmonBattleAction(v));
 								sendAttackMessage(act);
 								if (act.isBattleOver())
@@ -129,8 +139,9 @@ public final class GarmonBattleManager {
 		return new DetailedActionReaction("\u2139", "Info", "Check battle queue or enemy stats.", t -> {
 			var dam = new DetailedMenuMessage<>(new ActionMessage<>());
 			DetailedActionReaction battleQueue = new DetailedActionReaction("Battle Queue",
-					"Check the time until each creature's turn.", DetailedActionReaction.actionMessageAction(dam, t1 -> chan
-							.sendMessage("Battle Queue:").embed(GarmonUtils.printBattleQueue(battle).build()).queue()));
+					"Check the time until each creature's turn.",
+					DetailedActionReaction.actionMessageAction(dam, t1 -> chan.sendMessage("Battle Queue:")
+							.embed(GarmonUtils.printBattleQueue(battle).build()).queue()));
 			DetailedActionReaction back = new DetailedActionReaction("\u2B05", "Back", "Go back to attack menu.",
 					DetailedActionReaction.actionMessageAction(source));
 			dam.getReactions().add(battleQueue);
