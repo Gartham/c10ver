0f08b3f8ff3e397de5681183874af251fd85470c
Gartham <gartham@dusttoash.org>
2021-11-15 14:37:37 -0600

    Update GARPGHandler.java
    Added a quick method to resolve GarpgState from events.

--- src/gartham/c10ver/games/rpg/GARPGHandler.java
+++ src/gartham/c10ver/games/rpg/GARPGHandler.java
@@ -1,6 +1,7 @@
 package gartham.c10ver.games.rpg;
 
 import gartham.c10ver.Clover;
+import gartham.c10ver.economy.GARPGState;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class GARPGHandler {
@@ -10,9 +11,13 @@ public class GARPGHandler {
 	public GARPGHandler(Clover clover) {
 		this.clover = clover;
 	}
+	
+	private GARPGState resolve(GuildMessageReceivedEvent event) {
+		return clover.getEconomy().getUser(event.getAuthor().getId()).getGarpgState();
+	}
 
 	public void onEvent(GuildMessageReceivedEvent event) {
-
+		var rpgstate = resolve(event);
 	}
 
 }
