122ada348ee03e4d3402d861e68cac5509245042
Gartham <gartham@dusttoash.org>
2021-11-15 14:37:01 -0600

    Update GARPGState.java

--- src/gartham/c10ver/economy/GARPGState.java
+++ src/gartham/c10ver/economy/GARPGState.java
@@ -8,6 +8,7 @@ import gartham.c10ver.economy.users.EconomyUser;
 public class GARPGState extends SavablePropertyObject {
 
 	private final EconomyUser user;
+	private boolean active;
 
 	public GARPGState(File saveLocation, EconomyUser user) {
 		super(saveLocation);
@@ -16,6 +17,14 @@ public class GARPGState extends SavablePropertyObject {
 
 	public EconomyUser getUser() {
 		return user;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
 	}
 
 }
