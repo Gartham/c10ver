2f5fee23c65e7187c61be9958b12a54684375dd9
Gartham <gartham@dusttoash.org>
2021-11-16 00:32:17 -0600

    Filter more actions that should not be handled

--- src/gartham/c10ver/games/rpg/GARPGHandler.java
+++ src/gartham/c10ver/games/rpg/GARPGHandler.java
@@ -3,6 +3,9 @@ package gartham.c10ver.games.rpg;
 import gartham.c10ver.Clover;
 import gartham.c10ver.commands.InputProcessor;
 import gartham.c10ver.economy.GARPGState;
+import gartham.c10ver.games.rpg.creatures.Creature;
+import gartham.c10ver.games.rpg.creatures.Nymph;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -19,6 +22,7 @@ public class GARPGHandler implements EventListener {
 
 	private final InputProcessor<MessageReactionAddEvent> reactionProcessor = new InputProcessor<>();
 	private final InputProcessor<ButtonClickEvent> buttonProcessor = new InputProcessor<>();
+	private final Creature friend = new Nymph();
 
 	private final Clover clover;
 
@@ -35,7 +39,7 @@ public class GARPGHandler implements EventListener {
 		if (rpgstate.isActive()) {
 
 		} else {
-
+			GarmonUtils.sendAsCreature(friend, "Hi there.", event.getChannel());
 		}
 	}
 
@@ -43,28 +47,36 @@ public class GARPGHandler implements EventListener {
 	public void onEvent(GenericEvent event) {
 		if (event instanceof GuildMessageReceivedEvent) {
 			GuildMessageReceivedEvent e = (GuildMessageReceivedEvent) event;
-			if (!checkChannel(e.getGuild().getId(), e.getChannel().getId()))
+			if (!checkChannel(e.getGuild().getId(), e.getChannel().getId(), e.getAuthor()))
+				return;
+			if (e.isWebhookMessage())
 				return;
 			onEvent(e);
 		} else if (event instanceof MessageReactionAddEvent) {
 			MessageReactionAddEvent e = (MessageReactionAddEvent) event;
 			if (!e.isFromGuild())
 				return;
-			if (!checkChannel(e.getGuild().getId(), e.getChannel().getId()))
+			if (!checkChannel(e.getGuild().getId(), e.getChannel().getId(), e.retrieveUser().complete()))
 				return;
 			reactionProcessor.runInputHandlers(e);
 		} else if (event instanceof ButtonClickEvent) {
 			ButtonClickEvent e = (ButtonClickEvent) event;
 			if (!e.isFromGuild())
 				return;
-			if (!checkChannel(e.getGuild().getId(), e.getChannel().getId()))
+			if (!checkChannel(e.getGuild().getId(), e.getChannel().getId(), e.getUser()))
 				return;
 			buttonProcessor.runInputHandlers(e);
 		}
 	}
 
-	public boolean checkChannel(String serverID, String channelID) {
+	public boolean checkChannel(String serverID, String channelID, User author) {
 		if (!clover.getEconomy().hasServer(serverID))
+			return false;
+		if (author == null) {
+			System.err.println("Error in GaRPG channel, event causer is null.");
+			return false;
+		}
+		if (author.isBot() || author.isSystem())
 			return false;
 		return channelID.equals(clover.getEconomy().getServer(serverID).getRPGChannel());
 	}
