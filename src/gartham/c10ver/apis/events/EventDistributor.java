463c1f9984c0bea01981357f1bb2d12375027631
Gartham <gartham@dusttoash.org>
2021-11-16 14:40:45 -0600

    Add event handler functionality to EventDistributor

--- src/gartham/c10ver/apis/events/EventDistributor.java
+++ src/gartham/c10ver/apis/events/EventDistributor.java
@@ -1,7 +1,11 @@
 package gartham.c10ver.apis.events;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
+
+import org.alixia.javalibrary.util.Listmap;
 
 import gartham.c10ver.commands.InputProcessor;
 import net.dv8tion.jda.api.events.GenericEvent;
@@ -10,6 +14,20 @@ import net.dv8tion.jda.api.hooks.EventListener;
 public class EventDistributor implements EventListener {
 
 	private final Map<Class<?>, InputProcessor<?>> inputProcessors = new HashMap<>();
+	private final Listmap<Class<? extends GenericEvent>, Consumer<? super GenericEvent>, ArrayList<Consumer<? super GenericEvent>>> eventHandlers = Listmap
+			.arrayListMap();
+
+	public void registerHandler(Class<? extends GenericEvent> key, Consumer<? super GenericEvent> element) {
+		eventHandlers.putElement(key, element);
+	}
+
+	public void unregisterHandler(Class<? extends GenericEvent> key, Consumer<? super GenericEvent> element) {
+		eventHandlers.removeElement(key, element);
+	}
+
+	public boolean isHandlerRegistered(Class<? extends GenericEvent> key, Consumer<? super GenericEvent> element) {
+		return eventHandlers.containsElement(key, element);
+	}
 
 	@SuppressWarnings("unchecked")
 	public <E extends GenericEvent> InputProcessor<E> getProcessor(Class<E> eventType) {
@@ -21,12 +39,20 @@ public class EventDistributor implements EventListener {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onEvent(GenericEvent event) {
-		// TODO Run all input processors. InputProcessors are handled from lower classes
-		// to higher classes. If a lower handles, the uppers don't get to.
+
+		// Run all event handlers initially.
+		for (Class<?> c = event.getClass(); GenericEvent.class.isAssignableFrom(c); c = c.getSuperclass())
+			if (eventHandlers.containsKey(c))
+				for (var v : eventHandlers.get(c))
+					v.accept(event);
+
+		// InputProcessors are handled from lower classes to higher classes. If a lower
+		// handles, the uppers don't get to.
 		for (Class<?> c = event.getClass(); GenericEvent.class.isAssignableFrom(c); c = c.getSuperclass())
 			if (inputProcessors.containsKey(c))
 				if (((InputProcessor<GenericEvent>) inputProcessors.get(c)).runInputHandlers(event))
 					return;// runInputHandlers returns true if the input has already been handled.
+
 	}
 
 }
