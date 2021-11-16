4453aa5fe53cd19020cf44206a652f0222f2ba8b
Gartham <gartham@dusttoash.org>
2021-11-15 23:58:00 -0600

    Update GARPGHandler.java

--- src/gartham/c10ver/games/rpg/GARPGHandler.java
+++ src/gartham/c10ver/games/rpg/GARPGHandler.java
@@ -1,11 +1,18 @@
 package gartham.c10ver.games.rpg;
 
 import gartham.c10ver.Clover;
+import gartham.c10ver.commands.InputProcessor;
 import gartham.c10ver.economy.GARPGState;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 
 public class GARPGHandler implements EventListener {
+
+	private final InputProcessor<MessageReactionAddEvent> reactionProcessor = new InputProcessor<>();
+	private final InputProcessor<ButtonClickEvent> buttonProcessor = new InputProcessor<>();
 
 	private final Clover clover;
 
@@ -24,6 +31,12 @@ public class GARPGHandler implements EventListener {
 		} else {
 
 		}
+	}
+
+	@Override
+	public void onEvent(GenericEvent event) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
