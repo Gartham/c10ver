82d963b28c3c5834a1f11d2f2bd49a69c545e26e
Gartham <gartham@dusttoash.org>
2021-12-19 17:13:17 -0600

    Add methods for progression a GarmonBattle

--- src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonBattle.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonBattle.java
@@ -3,7 +3,6 @@ package gartham.c10ver.games.rpg.fighting.battles.app;
 import java.util.Collection;
 
 import gartham.c10ver.games.rpg.fighting.battles.api.Battle;
-import gartham.c10ver.games.rpg.fighting.battles.api.Team;
 import gartham.c10ver.games.rpg.fighting.fighters.Fighter;
 
 /**
@@ -40,13 +39,13 @@ import gartham.c10ver.games.rpg.fighting.fighters.Fighter;
  * This class removes the reliance on calling code between each call to
  * {@link #act(int)}, and so can function wholly "on its own," after being
  * started, (by simply calling each {@link Fighter}'s {@link GarmonTeam team}'s
- * {@link GarmonTeam#getController()} method to perform the operations normally
- * done by calling code in between calls to {@link #act(int)}). As a result,
- * this class provides multiple modes of operation, including modes executing on
- * their own, discrete threads. (Please note that this class is not multi-thread
- * safe; in the case of methods which launch their own thread, they are expected
- * to be the only thread to interface with the {@link GarmonBattle} until its
- * completion.)
+ * controller to perform the operations normally done by calling code in between
+ * calls to {@link #act(int)}). As a result, this class provides multiple modes
+ * of operation, including modes executing on their own, discrete threads.
+ * (Please note that this class is not multi-thread safe; in the case of methods
+ * which launch their own thread, they are expected to be the only thread to
+ * interface with the {@link GarmonBattle} until its completion.)
+ * </p>
  * 
  * @author Gartham
  *
@@ -63,6 +62,26 @@ public class GarmonBattle extends Battle<GarmonFighter, GarmonTeam> {
 
 	public int getFighterTicks(GarmonFighter fighter) {
 		return getTicks(fighter);
+	}
+
+	/**
+	 * Starts this battle on the executing thread. This method does not return until
+	 * the battle is completed. This method calls {@link #nextTurn()} until the
+	 * battle has concluded.
+	 */
+	public void start() {
+		while (!isOver()) {
+			
+		}
+	}
+
+	/**
+	 * Executes the next turn. This method calls the controller of the next fighter
+	 * to execute that fighter's turn, and then calls {@link #act(int)} to ready
+	 * this {@link Battle} object for the next fighter's turn.
+	 */
+	public void nextTurn() {
+
 	}
 
 //	@Override
