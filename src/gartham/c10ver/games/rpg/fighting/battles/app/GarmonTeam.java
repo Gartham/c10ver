b83148efd7609f4997b176b15055bf7f07aa43ef
Gartham <gartham@dusttoash.org>
2021-12-20 01:23:01 -0600

    Add Controller to GarmonTeam

--- src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonTeam.java
+++ src/gartham/c10ver/games/rpg/fighting/battles/app/GarmonTeam.java
@@ -2,28 +2,37 @@ package gartham.c10ver.games.rpg.fighting.battles.app;
 
 import java.util.Collection;
 
+import gartham.c10ver.games.rpg.fighting.battles.api.Controller;
 import gartham.c10ver.games.rpg.fighting.battles.api.Team;
 
 public class GarmonTeam extends Team<GarmonFighter> {
 	private final String name;
+	private final Controller<GarmonFighter> controller;
 
-	public GarmonTeam(String name, Collection<? extends GarmonFighter> members) {
+	public GarmonTeam(Iterable<? extends GarmonFighter> members, String name, Controller<GarmonFighter> controller) {
 		super(members);
 		this.name = name;
+		this.controller = controller;
+	}
+
+	public GarmonTeam(String name, Controller<GarmonFighter> controller, GarmonFighter... members) {
+		super(members);
+		this.name = name;
+		this.controller = controller;
+	}
+
+	public GarmonTeam(Collection<? extends GarmonFighter> members, String name, Controller<GarmonFighter> controller) {
+		super(members);
+		this.name = name;
+		this.controller = controller;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public GarmonTeam(String name, GarmonFighter... members) {
-		super(members);
-		this.name = name;
-	}
-
-	public GarmonTeam(String name, Iterable<? extends GarmonFighter> members) {
-		super(members);
-		this.name = name;
+	public Controller<GarmonFighter> getController() {
+		return controller;
 	}
 
 }
