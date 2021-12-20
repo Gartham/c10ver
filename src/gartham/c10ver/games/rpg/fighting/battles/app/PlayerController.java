1a16dafcd74bffb1ed14d2a524732e7ad85ddaa2
Gartham <gartham@dusttoash.org>
2021-12-20 02:43:48 -0600

    Create PlayerController.java

---
+++ src/gartham/c10ver/games/rpg/fighting/battles/app/PlayerController.java
@@ -0,0 +1,13 @@
+package gartham.c10ver.games.rpg.fighting.battles.app;
+
+import gartham.c10ver.games.rpg.fighting.battles.api.Controller;
+
+public class PlayerController implements Controller<GarmonFighter> {
+
+	@Override
+	public int move(GarmonFighter fighter) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
