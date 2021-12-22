6d502982ca9274eb068638f356b8386f593308f4
Gartham <gartham@dusttoash.org>
2021-12-22 03:14:40 -0600

    Add convenience methods to Actions

--- src/gartham/c10ver/response/actions/v2/Actions.java
+++ src/gartham/c10ver/response/actions/v2/Actions.java
@@ -1,6 +1,16 @@
 package gartham.c10ver.response.actions.v2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+
+import org.alixia.javalibrary.JavaTools;
+
+import gartham.c10ver.commands.InputProcessor;
+import gartham.c10ver.commands.consumers.MessageReactionInputConsumer;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 
 public final class Actions {
 
@@ -20,6 +30,54 @@ public final class Actions {
 
 	public static List<String> numberEmojis() {
 		return EMOJIS_LIST;
+	}
+
+	public static void attach(Message message, Consumer<String> handler,
+			InputProcessor<MessageReactionAddEvent> processor, String... reactions) {
+		attach(message, handler, processor, Arrays.asList(reactions));
+	}
+
+	public static void attach(Message message, Consumer<String> handler,
+			InputProcessor<MessageReactionAddEvent> processor, int numberReactions, String... reactions) {
+		attach(message, handler, processor, numberReactions, Arrays.asList(reactions));
+	}
+
+	public static void attach(Message message, Consumer<String> handler,
+			InputProcessor<MessageReactionAddEvent> processor, int numberReactions, List<String> reactions) {
+		attach(message, handler, processor, JavaTools.combine(new ArrayList<>(reactions.size() + numberReactions),
+				numberEmojis(numberReactions), reactions));
+	}
+
+	public static void attach(Message message, Consumer<String> handler,
+			InputProcessor<MessageReactionAddEvent> processor, List<String> reactions) {
+		reactions = new ArrayList<>(reactions);
+		reactions.replaceAll(Actions::normalizeEmoji);
+		final var x = reactions;
+		if (!reactions.isEmpty()) {
+			for (var s : reactions)
+				message.addReaction(s).queue();
+			processor.registerInputConsumer(
+					((MessageReactionInputConsumer<MessageReactionAddEvent>) (event, p, consumer) -> {
+						if (event.getMessageIdLong() != message.getIdLong())
+							return false;
+						for (var s : x) {
+							if (s.equalsIgnoreCase(event.getReactionEmote().getName())) {
+								handler.accept(s);
+								return true;
+							}
+						}
+						return false;
+					}).oneTime());
+		}
+	}
+
+	public static String normalizeEmoji(String emoji) {
+		if ((emoji = emoji.toLowerCase()).startsWith("a:"))
+			if (emoji.length() == 2)
+				throw new IllegalArgumentException();
+			else
+				emoji.substring(2);
+		return emoji;
 	}
 
 }
