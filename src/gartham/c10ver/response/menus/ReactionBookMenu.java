6729c4be30030877a402c1819f0f999c5b67e86c
Gartham <gartham@dusttoash.org>
2021-12-23 04:40:34 -0600

    Finish ReactionBookMenu (probably)

--- src/gartham/c10ver/response/menus/ReactionBookMenu.java
+++ src/gartham/c10ver/response/menus/ReactionBookMenu.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.function.Consumer;
 
 import gartham.c10ver.commands.InputProcessor;
+import gartham.c10ver.commands.consumers.MessageReactionInputConsumer;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
@@ -81,8 +82,46 @@ public class ReactionBookMenu {
 		this.pageHandler = pageHandler;
 	}
 
+	/**
+	 * Attaches this {@link ReactionBookMenu} (in its current state) to the provided
+	 * {@link Message}. This {@link ReactionBookMenu} can be safely modified and
+	 * used on a different {@link Message}. Modifications to this
+	 * {@link ReactionBookMenu} that are subsequent to an invocation of this method
+	 * will not affect that invocation of this method.
+	 * 
+	 * @param message The {@link Message} to attach the {@link ReactionBookMenu} to.
+	 */
 	public void attach(Message message) {
-		// TODO
+		final Set<String> x = new HashSet<>(reactions.size());
+		for (var s : reactions)
+			x.add(normalizeEmoji(s));
+		var h = handler;
+		if (!x.isEmpty()) {
+			for (var s : x)
+				message.addReaction(normalizeEmoji(s)).queue();
+
+			processor.registerInputConsumer(
+					((MessageReactionInputConsumer<MessageReactionAddEvent>) (event, p, consumer) -> {
+						if (event.getMessageIdLong() != message.getIdLong())
+							return false;
+						for (var s : x) {
+							if (s.equalsIgnoreCase(event.getReactionEmote().getName())) {
+								h.accept(s);
+								return true;
+							}
+						}
+						return false;
+					}).filter(target, message).oneTime());
+		}
+	}
+
+	public static String normalizeEmoji(String emoji) {
+		if ((emoji = emoji.toLowerCase()).startsWith("a:"))
+			if (emoji.length() == 2)
+				throw new IllegalArgumentException();
+			else
+				emoji.substring(2);
+		return emoji;
 	}
 
 }
