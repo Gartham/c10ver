3138f2735f1ecb72448ea4d9adc2bc54639e330c
Gartham <gartham@dusttoash.org>
2021-12-26 00:31:00 -0600

    Update ResponseUtils.java

--- src/gartham/c10ver/response/ResponseUtils.java
+++ src/gartham/c10ver/response/ResponseUtils.java
@@ -1,5 +1,7 @@
 package gartham.c10ver.response;
 
+import net.dv8tion.jda.api.interactions.components.Button;
+
 public final class ResponseUtils {
 	public static final String RIGHT_ALL = "\u23ED", LEFT_ALL = "\u23EE", RIGHT_ONE = "\u25B6", LEFT_ONE = "\u25C0";
 
@@ -13,6 +15,10 @@ public final class ResponseUtils {
 			else
 				emoji.substring(2);
 		return emoji;
+	}
+	
+	public static Button blockedButton(String id) {
+		return Button.secondary(id, "\u200B").asDisabled();
 	}
 
 }
