62f750a726796761d5625a3647ea4a9f1a2fd3c4
Gartham <gartham@dusttoash.org>
2021-12-26 16:00:31 -0600

    Modify connection API of DungeonRoom

--- src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
+++ src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
@@ -1,7 +1,11 @@
 package gartham.c10ver.games.rpg.dungeons;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import gartham.c10ver.games.rpg.rooms.RectangularRoom;
 
@@ -11,7 +15,12 @@ public class DungeonRoom<C> {
 
 	public DungeonRoom(RectangularRoom room, Map<C, DungeonRoom<C>> connections) {
 		this.room = room;
-		this.connections = connections;
+		this.connections = new HashMap<>(connections);
+	}
+
+	public DungeonRoom(RectangularRoom room) {
+		this.room = room;
+		this.connections = new HashMap<>();
 	}
 
 	public RectangularRoom getRoom() {
@@ -20,6 +29,28 @@ public class DungeonRoom<C> {
 
 	public Map<C, DungeonRoom<C>> getConnections() {
 		return Collections.unmodifiableMap(connections);
+	}
+
+	/**
+	 * Adds the specified connection to this {@link DungeonRoom}. (This should be
+	 * called on the provided room with the opposite direction in order to maintain
+	 * system consistency.)
+	 * 
+	 * @param dir  The direction of the wall that has the connection.
+	 * @param room The {@link DungeonRoom} connected to this {@link DungeonRoom}.
+	 */
+	public void addConnection(C dir, DungeonRoom<C> room) {
+		if (connections.containsKey(dir))
+			throw new RuntimeException("Entry already exists.");
+		connections.put(dir, room);
+	}
+
+	public List<C> getConnectionDirections() {
+		return new ArrayList<>(connections.keySet());
+	}
+
+	public Set<C> getConnectionDirectionsUnmodifiable() {
+		return Collections.unmodifiableSet(connections.keySet());
 	}
 
 }
