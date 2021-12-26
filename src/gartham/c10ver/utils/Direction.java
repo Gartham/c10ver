df8c82e4d436f3850ffc93676f2c2f682863eccf
Gartham <gartham@dusttoash.org>
2021-12-26 01:43:20 -0600

    Update Direction.java

--- src/gartham/c10ver/utils/Direction.java
+++ src/gartham/c10ver/utils/Direction.java
@@ -1,14 +1,28 @@
 package gartham.c10ver.utils;
 
+import java.util.List;
+
 public enum Direction {
 	UP, RIGHT, DOWN, LEFT;
 
 	private static final Direction[] values = values();
 
+	/**
+	 * Returns <code>true</code> if the wall of a square this {@link Direction} of
+	 * the center of a square runs horizontally.
+	 * 
+	 * @return <code>this == UP || this == DOWN</code>
+	 */
 	public boolean isHorizontal() {
 		return this == UP || this == DOWN;
 	}
 
+	/**
+	 * Returns <code>true</code> if the wall of a square this {@link Direction} of
+	 * the center of a square runs vertically.
+	 * 
+	 * @return <code>this == LEFT || this == RIGHT</code>
+	 */
 	public boolean isVertical() {
 		return !isHorizontal();
 	}
@@ -16,4 +30,7 @@ public enum Direction {
 	public Direction opposite() {
 		return values[(ordinal() + 2) % values.length];
 	}
+
+	public static final List<Direction> VALUES = List.of(values);
+
 }
