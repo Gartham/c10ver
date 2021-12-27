fab9ad0679597bcc8f84d1e638bb891bc891349d
Gartham <gartham@dusttoash.org>
2021-12-27 00:02:48 -0600

    Add RoomTraits type and claimed property to DungeonRoom

--- src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
+++ src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
@@ -1,5 +1,6 @@
 package gartham.c10ver.games.rpg.dungeons;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -7,6 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import gartham.c10ver.economy.RewardsOperation;
+import gartham.c10ver.games.rpg.fighting.battles.app.GarmonTeam;
 import gartham.c10ver.games.rpg.rooms.RectangularRoom;
 import gartham.c10ver.response.utils.DirectionSelector;
 import gartham.c10ver.utils.Direction;
@@ -14,15 +17,86 @@ import gartham.c10ver.utils.Direction;
 public class DungeonRoom {
 	private final RectangularRoom room;
 	private final Map<Direction, DungeonRoom> connections;
+	private final RoomTraits traits;
+	private boolean claimed;
 
-	public DungeonRoom(RectangularRoom room, Map<Direction, DungeonRoom> connections) {
-		this.room = room;
-		this.connections = new HashMap<>(connections);
+	public boolean isClaimed() {
+		return claimed;
 	}
 
-	public DungeonRoom(RectangularRoom room) {
+	public void setClaimed(boolean claimed) {
+		this.claimed = claimed;
+	}
+
+	public static class RoomTraits {
+		public enum Type {
+			EMPTY, CLOVES, LOOT, FIGHT;
+		}
+
+		private final Type type; // Changes the color of the Discord embed.
+		private final BigInteger cloves;
+		private final RewardsOperation loot;
+		private final GarmonTeam enemies;
+
+		public RoomTraits(BigInteger cloves) {
+			type = Type.CLOVES;
+			this.cloves = cloves;
+			loot = null;
+			enemies = null;
+		}
+
+		public RoomTraits(RewardsOperation loot) {
+			type = Type.LOOT;
+			this.loot = loot;
+			cloves = null;
+			enemies = null;
+		}
+
+		public RoomTraits(GarmonTeam enemies) {
+			type = Type.FIGHT;
+			this.enemies = enemies;
+			cloves = null;
+			loot = null;
+		}
+
+		public RoomTraits() {
+			type = Type.EMPTY;
+			cloves = null;
+			loot = null;
+			enemies = null;
+		}
+
+		public BigInteger cloves() {
+			return cloves;
+		}
+
+		public RewardsOperation loot() {
+			return loot;
+		}
+
+		public GarmonTeam enemies() {
+			return enemies;
+		}
+
+		public Type getType() {
+			return type;
+		}
+	}
+
+	public DungeonRoom(RectangularRoom room, Map<Direction, DungeonRoom> connections, RoomTraits traits) {
+		this.room = room;
+		this.connections = new HashMap<>(connections);
+		this.traits = traits;
+	}
+
+	public DungeonRoom(RectangularRoom room, RoomTraits traits) {
 		this.room = room;
 		this.connections = new HashMap<>();
+		this.traits = traits;
+	}
+
+	public RoomTraits getTraits() {
+		return traits;
 	}
 
 	public RectangularRoom getRoom() {
