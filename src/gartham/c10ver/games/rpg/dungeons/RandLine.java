a29b37bc7021dfcdd35d31ad9f3848dc65bf7aab
Gartham <gartham@dusttoash.org>
2021-12-30 01:48:13 -0600

    Create RandLines class

--- src/gartham/c10ver/games/rpg/dungeons/RandLine.java
+++ src/gartham/c10ver/games/rpg/dungeons/RandLine.java
@@ -1,7 +1,9 @@
 package gartham.c10ver.games.rpg.dungeons;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import gartham.c10ver.games.rpg.rooms.RectangularRoom.Graphic;
 
@@ -10,6 +12,14 @@ class RandLine implements Graphic {
 	private final String[] elements;
 
 	public RandLine(String[] elements, int position) {
+		this(position, Arrays.copyOf(elements, elements.length));
+	}
+
+	public RandLine(String... elements) {
+		this(-1, elements);
+	}
+
+	public RandLine(int position, String... elements) {
 		this.elements = elements;
 		this.position = position;
 	}
@@ -24,4 +34,32 @@ class RandLine implements Graphic {
 				elements.length);
 	}
 
+}
+
+class RandLines implements Graphic {
+	private final List<String[]> elements;
+
+	public RandLines(List<String[]> elements) {
+		this.elements = elements;
+	}
+
+	@Override
+	public void render(String[][] map) {
+		if (elements.size() > map.length - 2)
+			throw new IllegalStateException(
+					"There are too many random lines in this RandLines object to render to the specified tile map.");
+		List<Integer> positions = new ArrayList<>();
+		for (var rl : elements) {
+			int pos = (int) (Math.random() * (map.length - 2 - positions.size()) + 1);
+			var pp = Collections.binarySearch(positions, pos);
+			if (pp < 0)
+				pp = -pp;
+			pos += pp;// TODO Verify.
+			Collections.shuffle(Arrays.asList(elements));// String is an OBJECT, not a primitive type (so this works
+															// :-).
+			System.arraycopy(elements, 0, map[pos], (int) (Math.random() * (map[pos].length - rl.length - 1) + 1),
+					rl.length);
+			positions.add(-Collections.binarySearch(positions, pos) - 1, pos);
+		}
+	}
 }
