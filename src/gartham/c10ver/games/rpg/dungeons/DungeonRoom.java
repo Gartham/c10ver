989c02afacb9895ba48036d481c469cfc5c02643
Gartham <gartham@dusttoash.org>
2021-12-30 10:34:34 -0600

    Remove RoomTraits from DungeonRoom

--- src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
+++ src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
@@ -1,6 +1,5 @@
 package gartham.c10ver.games.rpg.dungeons;
 
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -8,12 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import gartham.c10ver.economy.RewardsOperation;
-import gartham.c10ver.games.rpg.fighting.battles.app.GarmonTeam;
 import gartham.c10ver.games.rpg.rooms.RectangularRoom;
 import gartham.c10ver.response.utils.DirectionSelector;
 import gartham.c10ver.utils.Direction;
-import net.dv8tion.jda.api.interactions.components.Button;
 
 public class DungeonRoom {
 	private final RectangularRoom room;
@@ -27,61 +23,6 @@ public class DungeonRoom {
 
 	public void setClaimed(boolean claimed) {
 		this.claimed = claimed;
-	}
-
-	public static class RoomTraits {
-		public enum Type {
-			EMPTY, CLOVES, LOOT, FIGHT;
-		}
-
-		private final Type type; // Changes the color of the Discord embed.
-		private final BigInteger cloves;
-		private final RewardsOperation loot;
-		private final GarmonTeam enemies;
-
-		public RoomTraits(BigInteger cloves) {
-			type = Type.CLOVES;
-			this.cloves = cloves;
-			loot = null;
-			enemies = null;
-		}
-
-		public RoomTraits(RewardsOperation loot) {
-			type = Type.LOOT;
-			this.loot = loot;
-			cloves = null;
-			enemies = null;
-		}
-
-		public RoomTraits(GarmonTeam enemies) {
-			type = Type.FIGHT;
-			this.enemies = enemies;
-			cloves = null;
-			loot = null;
-		}
-
-		public RoomTraits() {
-			type = Type.EMPTY;
-			cloves = null;
-			loot = null;
-			enemies = null;
-		}
-
-		public BigInteger cloves() {
-			return cloves;
-		}
-
-		public RewardsOperation loot() {
-			return loot;
-		}
-
-		public GarmonTeam enemies() {
-			return enemies;
-		}
-
-		public Type getType() {
-			return type;
-		}
 	}
 
 	public DungeonRoom(RectangularRoom room, Map<Direction, DungeonRoom> connections) {
