d08894491ee752f6a590fcee362f7733b50c6e56
Gartham <gartham@dusttoash.org>
2021-12-30 15:37:19 -0600

    Update DungeonRoom.java

--- src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
+++ src/gartham/c10ver/games/rpg/dungeons/DungeonRoom.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 import gartham.c10ver.games.rpg.rooms.RectangularRoom;
+import gartham.c10ver.games.rpg.rooms.RectangularRoom.Opening;
 import gartham.c10ver.response.utils.DirectionSelector;
 import gartham.c10ver.utils.Direction;
 
@@ -39,30 +40,112 @@ public class DungeonRoom {
 		return room;
 	}
 
-	public Map<Direction, DungeonRoom> getConnections() {
-		return Collections.unmodifiableMap(connections);
-	}
-
 	/**
 	 * Adds the specified connection to this {@link DungeonRoom}. (This should be
 	 * called on the provided room with the opposite direction in order to maintain
-	 * system consistency.)
+	 * system consistency.) Without calling
+	 * <code>room.addConnection(dir.opposite(), this);</code> (with
+	 * <code>room</code> being the {@link DungeonRoom} provided to this method,
+	 * <code>dir</code> being the direction provided to this method, and
+	 * <code>this</code> referring to this object), there will essentially be a
+	 * "one-way" path from this {@link DungeonRoom} to the provided
+	 * {@link DungeonRoom}.
 	 * 
-	 * @param dir  The direction of the wall that has the connection.
-	 * @param room The {@link DungeonRoom} connected to this {@link DungeonRoom}.
+	 * @param room    The {@link DungeonRoom} connected to this {@link DungeonRoom}.
+	 * @param opening The opening to add.
 	 */
-	public void addConnection(Direction dir, DungeonRoom room) {
-		if (connections.containsKey(dir))
-			throw new RuntimeException("Entry already exists.");
-		connections.put(dir, room);
+	public Path addPath(DungeonRoom room, Opening opening) {
+		var p = new Path(room, opening);
+		p.put();
+		return p;
+	}
+
+	/**
+	 * This method is the same as {@link #addPath(DungeonRoom, Opening)} except that
+	 * it generates a randomly sized, randomly positioned {@link Opening} that fits
+	 * in the wall specified by the provided {@link Direction}.
+	 * 
+	 * @param dir  The {@link Direction} for the wall to face.
+	 * @param room The room connected to this room.
+	 * @return The new {@link Path} created.
+	 */
+	public Path addRandomPath(Direction dir, DungeonRoom room) {
+		var size = dir.isHorizontal() ? getRoom().getHeight() : getRoom().getWidth();
+		var gw = (int) Math.random() * (size - 4) + 1;
+		
+		// Position should be Math.random() * (size - gw - 4 + 1).
+		return addPath(room, getRoom().createOpening(dir, gw, (int) Math.random() * (size - gw - 3)));
+	}
+
+	public class Path {
+		private final DungeonRoom to;
+		private final Opening opening;
+
+		public void remove() {
+			connections.remove(opening.getDirection(), to);
+		}
+
+		public void put() {
+			if (connections.containsKey(opening.getDirection()))
+				throw new RuntimeException("Entry already exists.");
+			connections.put(opening.getDirection(), to);
+		}
+
+		public DungeonRoom getTo() {
+			return to;
+		}
+
+		public Opening getOpening() {
+			return opening;
+		}
+
+		public Path(DungeonRoom to, Opening opening) {
+			this.to = to;
+			this.opening = opening;
+		}
+
+		public DungeonRoom getFrom() {
+			return DungeonRoom.this;
+		}
+	}
+
+	public static class Connection {
+		private final Path first, second;
+
+		public Connection(Path to, Path from) {
+			this.second = to;
+			this.first = from;
+		}
+
+		public Path getSecond() {
+			return second;
+		}
+
+		public Path getFirst() {
+			return first;
+		}
+
+		public void put() {
+			if (second.getFrom().connections.containsKey(second.opening.getDirection())
+					|| first.getFrom().connections.containsKey(first.opening.getDirection()))
+				throw new IllegalStateException(
+						"At least one of the DungeonRooms referred to by this Connection object is already connected.");
+			first.getFrom().connections.put(first.opening.getDirection(), first.getTo());
+			second.getFrom().connections.put(second.opening.getDirection(), second.getTo());
+		}
+
+		public void remove() {
+			first.remove();
+			second.remove();
+		}
 	}
 
 	/**
 	 * Connects this {@link DungeonRoom} to the specified {@link DungeonRoom}. This
-	 * is similar to {@link #addConnection(Direction, DungeonRoom)}, but this method
+	 * is similar to {@link #addPath(DungeonRoom, Opening)}, but this method
 	 * maintains consistency of the dungeon by calling
-	 * {@link #addConnection(Direction, DungeonRoom)} on the other dungeon room with
-	 * the arguments {@link Direction#opposite()} and <code>this</code>. This method
+	 * {@link #addPath(DungeonRoom, Opening)} on the other dungeon room with the
+	 * arguments {@link Direction#opposite()} and <code>this</code>. This method
 	 * will fail if either {@link DungeonRoom} has a connection in the requisite
 	 * directions already.
 	 * 
@@ -71,13 +154,17 @@ public class DungeonRoom {
 	 *              {@link DungeonRoom} connects to.
 	 * @param other The other {@link DungeonRoom}.
 	 */
-	public void connect(Direction dir, DungeonRoom other) {
-		if (connections.containsKey(dir) || other.connections.containsKey(dir.opposite()))
-			throw new IllegalStateException("At least one of the DungeonRooms is already connected.");
-		connections.put(dir, other);
-		other.connections.put(dir.opposite(), this);
+	public Connection connect(Direction dir, DungeonRoom other) {
+		return new Connection(addRandomPath(dir, other), other.addRandomPath(dir.opposite(), this));
 	}
 
+	/**
+	 * Gets a new {@link List} of {@link Direction}s in which there are connected
+	 * dungeons.
+	 * 
+	 * @return A {@link List} of all the {@link Direction}s in which there are
+	 *         connected dungeons.
+	 */
 	public List<Direction> getConnectionDirections() {
 		return new ArrayList<>(connections.keySet());
 	}
