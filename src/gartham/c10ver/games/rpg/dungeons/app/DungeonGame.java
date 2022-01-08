a8b24e6218617538509234de97f2a3051a0c2b45
Gartham <gartham@dusttoash.org>
2022-01-08 05:48:47 -0600

    Update DungeonGame.java

--- src/gartham/c10ver/games/rpg/dungeons/app/DungeonGame.java
+++ src/gartham/c10ver/games/rpg/dungeons/app/DungeonGame.java
@@ -1,12 +1,36 @@
 package gartham.c10ver.games.rpg.dungeons.app;
 
 import java.awt.Color;
+import java.math.BigInteger;
 
 import gartham.apps.garthchat.api.execution.Action;
+import gartham.c10ver.Clover;
 import gartham.c10ver.commands.InputProcessor;
+import gartham.c10ver.commands.consumers.InputConsumer;
+import gartham.c10ver.economy.RewardsOperation;
+import gartham.c10ver.economy.items.ItemBunch;
+import gartham.c10ver.economy.items.valuables.VoteToken;
+import gartham.c10ver.economy.users.EconomyUser;
+import gartham.c10ver.economy.users.EconomyUser.Receipt;
+import gartham.c10ver.games.rpg.dungeons.Dungeon;
+import gartham.c10ver.games.rpg.dungeons.DungeonRoom;
+import gartham.c10ver.games.rpg.dungeons.EnemyRoom;
+import gartham.c10ver.games.rpg.dungeons.LootRoom;
+import gartham.c10ver.games.rpg.fighting.battles.app.CreatureAI;
+import gartham.c10ver.games.rpg.fighting.battles.app.GarmonBattle;
+import gartham.c10ver.games.rpg.fighting.battles.app.GarmonTeam;
+import gartham.c10ver.games.rpg.fighting.battles.app.PlayerController;
+import gartham.c10ver.games.rpg.fighting.battles.app.PlayerFighter;
 import gartham.c10ver.response.menus.ButtonBook;
+import gartham.c10ver.response.utils.DirectionSelector;
+import gartham.c10ver.utils.Utilities;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
@@ -32,8 +56,9 @@ public class DungeonGame {
 	}
 
 	private final User target;
-	private final MessageChannel channel;
+	private final TextChannel channel;
 	private final InputProcessor<ButtonClickEvent> buttonProcessor;
+	private final Clover clover;
 
 	/**
 	 * @param target          The target {@link User} to prompt. (This {@link User}
@@ -45,10 +70,157 @@ public class DungeonGame {
 	 *                        {@link InputProcessor} receives the clicks, and will
 	 *                        then result in a page number.
 	 */
-	public DungeonGame(User target, MessageChannel channel, InputProcessor<ButtonClickEvent> buttonProcessor) {
+	public DungeonGame(User target, TextChannel channel, InputProcessor<ButtonClickEvent> buttonProcessor,
+			Clover clover) {
 		this.target = target;
 		this.channel = channel;
 		this.buttonProcessor = buttonProcessor;
+		this.clover = clover;
+	}
+
+	public void start(DungeonCover... dungeons) {
+		EmbedBuilder eb = new EmbedBuilder();
+		eb.setColor(new Color(0x5E2A03));
+		eb.setTitle("World Selector").setDescription("Select a world to explore " + target.getAsMention() + ":")
+				.setImage(dungeons[0].graphic).setFooter("Page 1 of " + dungeons.length);
+
+		var bb = new ButtonBook(buttonProcessor);
+		bb.add("\u2705");
+		bb.setTarget(target);
+		bb.setMaxPage(dungeons.length - 1);
+		bb.setEdgeButtons(true);
+		bb.setPageHandler((t, e) -> e.getHook().editOriginalEmbeds(
+				eb.setImage(dungeons[t].graphic).setFooter("Page " + (t + 1) + " of " + (bb.getMaxPage() + 1)).build())
+				.queue());
+
+		var abb = bb.attachAndSend(channel.sendMessageEmbeds(eb.build()));
+		abb.setHandler(t -> {
+			t.deferEdit().complete();
+			abb.unregister();
+			startDungeon(t);
+		});
+	}
+
+	Message t;
+
+	private void startDungeon(ButtonClickEvent e) {
+		t = e.getMessage();// This is broken lmao.
+//		try {
+//			Thread.sleep((long) (Math.random() * 3500 + 1500));
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+
+		var dungeon = Dungeon.simpleEasyDungeon();
+		var initialRoom = dungeon.getInitialRoom();
+
+		var dirsel = DirectionSelector.directionSelector();
+		dirsel.disableDirections();
+		for (var d : initialRoom.getConnectionDirectionsUnmodifiable())
+			dirsel.enable(d);
+
+		var inp = new InputConsumer<ButtonClickEvent>() {
+
+			DungeonRoom currentRoom = initialRoom;
+
+			@Override
+			public boolean consume(ButtonClickEvent event, InputProcessor<? extends ButtonClickEvent> processor,
+					InputConsumer<ButtonClickEvent> consumer) {
+				if (event.getMessageIdLong() != t.getIdLong())
+					return false;
+				if (event.getUser().getIdLong() != target.getIdLong()) {
+					event.reply("That's not for you. >:(").setEphemeral(true).queue();
+					return true;
+				}
+
+				if (event.getComponentId().equals("act")) {
+					if (currentRoom.isClaimed()) {
+						event.reply(
+								"You already collected the loot from this room... (You're not supposed to be able to click that button again!)")
+								.setEphemeral(true).queue();
+						return true;
+					}
+					currentRoom.setClaimed(true);
+					var user = clover.getEconomy().getUser(target.getId());
+					Receipt receipt;
+
+					var lr = (LootRoom) currentRoom;
+					receipt = user.reward(lr.getRewards().autoSetMultipliers(user,
+							channel.getType().isGuild() ? ((GuildChannel) channel).getGuild() : null));
+
+					currentRoom.prepare(dirsel, "act");
+					event.editComponents(dirsel.actionRows()).queue();
+					t.reply(target.getAsMention() + ", you earned:\n\n" + Utilities.listRewards(receipt)).queue();
+
+					// Resend message? Send ephemeral rewards?
+				} else {
+					var dir = DirectionSelector.getDirectionSelected(event);
+					currentRoom = currentRoom.getRoom(dir);
+					if (currentRoom == null) {// Player moved in the wrong direction.
+						event.reply("You are not supposed to be able to click that!").setEphemeral(true).queue();
+						processor.removeInputConsumer(consumer);
+					} else if (!currentRoom.isClaimed() && currentRoom instanceof EnemyRoom) {
+						// TODO Player has encountered a fight. Force them into a
+						// battle before they can continue.
+
+						var room = (EnemyRoom) currentRoom;
+
+						currentRoom.setClaimed(true);
+
+						GarmonTeam player = new GarmonTeam(target.getAsTag(),
+								new PlayerFighter(target.getName(), target.getEffectiveAvatarUrl(),
+										BigInteger.valueOf(25), BigInteger.valueOf(100), BigInteger.valueOf(100),
+										BigInteger.valueOf(25), BigInteger.valueOf(5)));
+						var team = room.getEnemies();
+						GarmonBattle battle = new GarmonBattle(player, team);
+						player.setController(new PlayerController(battle, clover, target, channel));
+						team.setController(new CreatureAI(battle, channel));
+
+						dirsel.reset();
+						dirsel.disableDirections();
+						event.editComponents(dirsel.actionRows()).setContent("**You got into a fight!!!**")
+								.setEmbeds(new EmbedBuilder().setColor(new Color(0xFF0000))
+										.setTitle("Room #" + (dungeon.index(currentRoom) + 1))
+										.setDescription("```" + currentRoom.getRoom().tilemapString() + "```")
+										.setFooter("Choose a path.").build())
+								.queue();
+						battle.startAsync(true, winner -> {
+
+							if (winner == player) {
+								EconomyUser user = clover.getEconomy().getUser(target.getId());
+								RewardsOperation rewop = RewardsOperation.build(user, channel.getGuild(),
+										BigInteger.valueOf((long) (Math.random() * 142 + 25)),
+										new ItemBunch<>(new VoteToken(
+												gartham.c10ver.economy.items.valuables.VoteToken.Type.NORMAL)));
+								currentRoom.prepare(dirsel, "act");
+								channel.sendMessage(target.getAsMention() + ", you won the fight!\nYou earned:\n\n"
+										+ Utilities.listRewards(user.reward(rewop)))
+										.setEmbeds(new EmbedBuilder().setColor(new Color(0xFF00))
+												.setTitle("Room #" + (dungeon.index(currentRoom) + 1))
+												.setDescription("```" + currentRoom.getRoom().tilemapString() + "```")
+												.setFooter("Choose a path.").build())
+										.setActionRows(dirsel.actionRows()).queue(x -> t = x);
+							}
+						});
+
+					} else {
+						currentRoom.prepare(dirsel, "act");
+						event.editMessageEmbeds(new EmbedBuilder().setTitle("Room #" + (dungeon.index(currentRoom) + 1))
+								.setDescription("```" + currentRoom.getRoom().tilemapString() + "```")
+								.setFooter("Choose a path.").build()).setActionRows(dirsel.actionRows()).setContent("")
+								.queue();
+					}
+				}
+				return true;
+
+			}
+		};
+
+		clover.getEventHandler().getButtonClickProcessor().registerInputConsumer(inp);
+
+		t.editMessage(new MessageBuilder().setEmbeds(new EmbedBuilder().setTitle("W1-1")
+				.setDescription("```" + initialRoom.getRoom().tilemapString() + "```").setFooter("Choose a path.")
+				.build()).setActionRows(dirsel.actionRows()).build()).queue();
 	}
 
 	/**
@@ -108,166 +280,6 @@ public class DungeonGame {
 //		abb.setHandler(t -> {
 //
 //			t.getInteraction().editComponents(abb.disabledButtonView()).queue();
-//			t.getChannel().sendMessage("Generating a randomized dungeon...").queue(new Consumer<>() {
-//
-//				private Message t;
-//
-//				@Override
-//				public void accept(Message msg) {
-//					t = msg;
-//					t.getChannel().sendTyping().queue(new Consumer<>() {
-//
-//						@Override
-//						public void accept(Void v) {
-//							try {
-//								Thread.sleep((long) (Math.random() * 3500 + 1500));
-//							} catch (InterruptedException e) {
-//								e.printStackTrace();
-//							}
-//
-//							var dungeon = Dungeon.simpleEasyDungeon();
-//							var initialRoom = dungeon.getInitialRoom();
-//
-//							var dirsel = DirectionSelector.directionSelector();
-//							dirsel.disableDirections();
-//							for (var d : initialRoom.getConnectionDirectionsUnmodifiable())
-//								dirsel.enable(d);
-//
-//							var inp = new InputConsumer<ButtonClickEvent>() {
-//
-//								DungeonRoom currentRoom = initialRoom;
-//
-//								@Override
-//								public boolean consume(ButtonClickEvent event,
-//										InputProcessor<? extends ButtonClickEvent> processor,
-//										InputConsumer<ButtonClickEvent> consumer) {
-//									if (event.getMessageIdLong() != t.getIdLong())
-//										return false;
-//									if (event.getUser().getIdLong() != inv.event.getAuthor().getIdLong()) {
-//										event.reply("That's not for you. >:(").setEphemeral(true).queue();
-//										return true;
-//									}
-//
-//									if (event.getComponentId().equals("act")) {
-//										if (currentRoom.isClaimed()) {
-//											event.reply(
-//													"You already collected the loot from this room... (You're not supposed to be able to click that button again!)")
-//													.setEphemeral(true).queue();
-//											return true;
-//										}
-//										currentRoom.setClaimed(true);
-//										var user = clover.getEconomy().getUser(inv.event.getAuthor().getId());
-//										Receipt receipt;
-//
-//										var lr = (LootRoom) currentRoom;
-//										receipt = user.reward(lr.getRewards().autoSetMultipliers(user,
-//												inv.event.isFromGuild() ? inv.event.getGuild() : null));
-//
-//										currentRoom.prepare(dirsel, "act");
-//										event.editComponents(dirsel.actionRows()).queue();
-//										t.reply(inv.event.getAuthor().getAsMention() + ", you earned:\n\n"
-//												+ Utilities.listRewards(receipt)).queue();
-//
-//										// Resend message? Send ephemeral rewards?
-//									} else {
-//										var dir = DirectionSelector.getDirectionSelected(event);
-//										currentRoom = currentRoom.getRoom(dir);
-//										if (currentRoom == null) {// Player moved in the wrong direction.
-//											event.reply("You are not supposed to be able to click that!")
-//													.setEphemeral(true).queue();
-//											processor.removeInputConsumer(consumer);
-//										} else if (!currentRoom.isClaimed() && currentRoom instanceof EnemyRoom) {
-//											// TODO Player has encountered a fight. Force them into a
-//											// battle before they can continue.
-//
-//											var room = (EnemyRoom) currentRoom;
-//
-//											currentRoom.setClaimed(true);
-//
-//											GarmonTeam player = new GarmonTeam(inv.event.getAuthor().getAsTag(),
-//													new PlayerFighter(inv.event.getAuthor().getName(),
-//															inv.event.getAuthor().getEffectiveAvatarUrl(),
-//															BigInteger.valueOf(25), BigInteger.valueOf(100),
-//															BigInteger.valueOf(100), BigInteger.valueOf(25),
-//															BigInteger.valueOf(5)));
-//											var team = room.getEnemies();
-//											GarmonBattle battle = new GarmonBattle(player, team);
-//											player.setController(new PlayerController(battle, clover,
-//													inv.event.getAuthor(), inv.event.getTextChannel()));
-//											team.setController(new CreatureAI(battle, inv.event.getTextChannel()));
-//
-//											dirsel.reset();
-//											dirsel.disableDirections();
-//											event.editComponents(dirsel.actionRows())
-//													.setContent("**You got into a fight!!!**")
-//													.setEmbeds(new EmbedBuilder().setColor(new Color(0xFF0000))
-//															.setTitle("W" + (abb.getPage() + 1) + "-"
-//																	+ (dungeon.index(currentRoom) + 1))
-//															.setDescription("```"
-//																	+ currentRoom.getRoom().tilemapString() + "```")
-//															.setFooter("Choose a path.").build())
-//													.queue();
-//											battle.startAsync(true, new Consumer<GarmonTeam>() {
-//
-//												@Override
-//												public void accept(GarmonTeam winner) {
-//
-//													if (winner == player) {
-//														EconomyUser user = clover.getEconomy()
-//																.getUser(inv.event.getAuthor().getId());
-//														RewardsOperation rewop = RewardsOperation.build(user,
-//																inv.event.getGuild(),
-//																BigInteger.valueOf((long) (Math.random() * 142 + 25)),
-//																new ItemBunch<>(new VoteToken(
-//																		gartham.c10ver.economy.items.valuables.VoteToken.Type.NORMAL)));
-//														currentRoom.prepare(dirsel, "act");
-//														inv.event.getChannel()
-//																.sendMessage(inv.event.getAuthor().getAsMention()
-//																		+ ", you won the fight!\nYou earned:\n\n"
-//																		+ Utilities.listRewards(user.reward(rewop)))
-//																.setEmbeds(new EmbedBuilder()
-//																		.setColor(new Color(0xFF00))
-//																		.setTitle("W" + (abb.getPage() + 1) + "-"
-//																				+ (dungeon.index(currentRoom) + 1))
-//																		.setDescription("```"
-//																				+ currentRoom.getRoom().tilemapString()
-//																				+ "```")
-//																		.setFooter("Choose a path.").build())
-//																.setActionRows(dirsel.actionRows()).queue(x -> t = x);
-//													}
-//												}
-//											});
-//
-//										} else {
-//											currentRoom.prepare(dirsel, "act");
-//											event.editMessageEmbeds(new EmbedBuilder()
-//													.setTitle("W" + (abb.getPage() + 1) + "-"
-//															+ (dungeon.index(currentRoom) + 1))
-//													.setDescription(
-//															"```" + currentRoom.getRoom().tilemapString() + "```")
-//													.setFooter("Choose a path.").build())
-//													.setActionRows(dirsel.actionRows()).setContent("").queue();
-//										}
-//									}
-//									return true;
-//
-//								}
-//							};
-//
-//							clover.getEventHandler().getButtonClickProcessor().registerInputConsumer(inp);
-//
-//							t.editMessage(
-//									new MessageBuilder()
-//											.setEmbeds(new EmbedBuilder().setTitle("W1-1")
-//													.setDescription(
-//															"```" + initialRoom.getRoom().tilemapString() + "```")
-//													.setFooter("Choose a path.").build())
-//											.setActionRows(dirsel.actionRows()).build())
-//									.queue();
-//						}
-//					});
-//				}
-//			});
 //
 //		});
 
