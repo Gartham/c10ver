728ed8f0292a3340ca38206e6d6d7e6b507e0848
Gartham <gartham@dusttoash.org>
2022-01-15 08:00:57 -0600

    Update InventoryCommand.java

--- src/gartham/c10ver/processing/commands/InventoryCommand.java
+++ src/gartham/c10ver/processing/commands/InventoryCommand.java
@@ -1,8 +1,11 @@
 package gartham.c10ver.processing.commands;
 
+import java.awt.Color;
+import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import org.alixia.javalibrary.JavaTools;
 import org.alixia.javalibrary.strings.matching.Matching;
 
 import gartham.c10ver.Clover;
@@ -10,6 +13,7 @@ import gartham.c10ver.commands.CommandInvocation;
 import gartham.c10ver.commands.MatchBasedCommand;
 import gartham.c10ver.data.PropertyObject;
 import gartham.c10ver.economy.items.Inventory.Entry;
+import gartham.c10ver.economy.items.ItemCategory;
 import gartham.c10ver.economy.items.UserInventory;
 import gartham.c10ver.utils.Utilities;
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -28,102 +32,129 @@ public class InventoryCommand extends MatchBasedCommand {
 		this.clover = clover;
 	}
 
+	private void displayRoot(CommandInvocation inv) {
+		EmbedBuilder eb = new EmbedBuilder();
+		UserInventory inventory = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
+		if (inventory.isEmpty()) {
+			eb.appendDescription("Your inventory is empty!");
+			eb.setColor(new Color(0xc20000));
+		} else {
+			Map<ItemCategory, BigInteger> ics = new HashMap<>();
+			for (var e : inventory)
+				for (var is : e)
+					if (ics.containsKey(is.getItem().getCategory()))
+						ics.put(is.getItem().getCategory(), ics.get(is.getItem().getCategory()).add(is.count()));
+					else
+						ics.put(is.getItem().getCategory(), is.count());
+
+			eb.appendDescription(
+					"You have **" + Utilities.formatNumber(inventory.getTotalItemCount()) + "** items.\n\n");
+
+			for (var i : ics.entrySet())
+				eb.appendDescription(i.getKey().getIcon() + ' ' + i.getKey().getDisplayName() + " **("
+						+ Utilities.formatNumber(i.getValue()) + ")**\n");
+		}
+		inv.event.getChannel().sendMessageEmbeds(eb.build()).queue();
+	}
+
 	@Override
 	public void exec(CommandInvocation inv) {
 
-		final UserInventory invent;
-		final String type;
-		int page;
-		ENTRIES: {
-			if (inv.args.length == 1) {
-				// The argument should be either an item type or a page.
-				try {
-					page = Integer.parseInt(inv.args[0]);
-				} catch (NumberFormatException e) {
-					invent = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
-					type = inv.args[0];
-					page = 1;
-					break ENTRIES;
-				}
-				if (page < 1) {
-					inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " `"
-							+ Utilities.strip(inv.args[0]) + "` is not a valid page.").queue();
-					return;
-				}
-			} else if (inv.args.length == 0)
-				page = 1;
-			else if (inv.args.length == 2) {
-				PARSE_PAGE: {
-					try {
-						page = Integer.parseInt(inv.args[1]);
-						if (page > 0)
-							break PARSE_PAGE;
-					} catch (NumberFormatException e) {
-					}
-					inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " `"
-							+ Utilities.strip(inv.args[1]) + "` is not a valid page.").queue();
-					return;
-				}
+		displayRoot(inv);
 
-				type = inv.args[0];
-				invent = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
-				break ENTRIES;
-			} else {
-				inv.event.getChannel()
-						.sendMessage(inv.event.getAuthor().getAsMention() + " that command doesn't accept "
-								+ inv.args.length + " arguments." + (inv.args.length > 10 ? " >:(" : ""))
-						.queue();
-				return;
-			}
-
-			invent = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
-			var pageItems = invent.getPage(page, 9);
-			int maxPage = invent.maxPage(9);
-			if (pageItems == null) {
-				inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " you only have `" + maxPage
-						+ (maxPage == 1 ? "` page" : "` pages") + " in your inventory!").queue();
-			} else {
-				EmbedBuilder eb = new EmbedBuilder();
-
-				eb.setAuthor(inv.event.getAuthor().getAsTag() + "'s Inventory", null,
-						inv.event.getAuthor().getEffectiveAvatarUrl());
-				eb.setDescription('*' + inv.event.getAuthor().getAsMention() + " has `" + invent.getEntryCount() + "` "
-						+ (invent.getEntryCount() == 1 ? "type of item" : "different types of items") + " and `"
-						+ invent.getTotalItemCount() + "` total items.*\n\u200B");
-				printEntries(pageItems, eb);
-				eb.addField("", "You have **" + maxPage + "** page" + (maxPage == 1 ? "" : "s") + " in your inventory.",
-						false);
-//				eb.setFooter(
-//						"You have " + maxPage + " page" + (maxPage == 1 ? "" : "s") + " in your inventory.");
-				inv.event.getChannel().sendMessageEmbeds(eb.build()).queue();
-			}
-			return;
-		}
-
-		var entry = invent.get(type);
-		if (entry == null)
-			inv.event.getChannel()
-					.sendMessage(inv.event.getAuthor().getAsMention() + " you don't have any items of that type!")
-					.queue();
-		else {
-			EmbedBuilder eb = new EmbedBuilder();
-			eb.setAuthor(inv.event.getAuthor().getAsTag() + "'s Inventory: " + entry.getName(), null,
-					inv.event.getAuthor().getEffectiveAvatarUrl());
-			eb.setDescription('*' + inv.event.getAuthor().getAsMention() + " has `" + entry.getTotalCount()
-					+ "` of this item.*\n\u200B");
-			int maxPage = JavaTools.maxPage(9, entry.getStacks());
-			if (page > maxPage) {
-				inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " you only have `" + maxPage
-						+ (maxPage == 1 ? "` page" : "` pages") + " of that item in your inventory!").queue();
-				return;
-			}
-			List<? extends Entry<?>.ItemStack> list = JavaTools.paginate(page, 9, entry.getStacks());
-			printStacks(list, eb);
-			eb.addField("", "You have **" + maxPage + "** page" + (maxPage == 1 ? "" : "s")
-					+ " of this item in your inventory.", false);
-//			eb.setFooter("You have " + maxPage + " page" + (maxPage == 1 ? "" : "s") + " in your inventory.");
-			inv.event.getChannel().sendMessageEmbeds(eb.build()).queue();
-		}
+//		final UserInventory invent;
+//		final String type;
+//		int page;
+//		ENTRIES: {
+//			if (inv.args.length == 1) {
+//				// The argument should be either an item type or a page.
+//				try {
+//					page = Integer.parseInt(inv.args[0]);
+//				} catch (NumberFormatException e) {
+//					invent = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
+//					type = inv.args[0];
+//					page = 1;
+//					break ENTRIES;
+//				}
+//				if (page < 1) {
+//					inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " `"
+//							+ Utilities.strip(inv.args[0]) + "` is not a valid page.").queue();
+//					return;
+//				}
+//			} else if (inv.args.length == 0)
+//				page = 1;
+//			else if (inv.args.length == 2) {
+//				PARSE_PAGE: {
+//					try {
+//						page = Integer.parseInt(inv.args[1]);
+//						if (page > 0)
+//							break PARSE_PAGE;
+//					} catch (NumberFormatException e) {
+//					}
+//					inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " `"
+//							+ Utilities.strip(inv.args[1]) + "` is not a valid page.").queue();
+//					return;
+//				}
+//
+//				type = inv.args[0];
+//				invent = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
+//				break ENTRIES;
+//			} else {
+//				inv.event.getChannel()
+//						.sendMessage(inv.event.getAuthor().getAsMention() + " that command doesn't accept "
+//								+ inv.args.length + " arguments." + (inv.args.length > 10 ? " >:(" : ""))
+//						.queue();
+//				return;
+//			}
+//
+//			invent = clover.getEconomy().getInventory(inv.event.getAuthor().getId());
+//			var pageItems = invent.getPage(page, 9);
+//			int maxPage = invent.maxPage(9);
+//			if (pageItems == null) {
+//				inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " you only have `" + maxPage
+//						+ (maxPage == 1 ? "` page" : "` pages") + " in your inventory!").queue();
+//			} else {
+//				EmbedBuilder eb = new EmbedBuilder();
+//
+//				eb.setAuthor(inv.event.getAuthor().getAsTag() + "'s Inventory", null,
+//						inv.event.getAuthor().getEffectiveAvatarUrl());
+//				eb.setDescription('*' + inv.event.getAuthor().getAsMention() + " has `" + invent.getEntryCount() + "` "
+//						+ (invent.getEntryCount() == 1 ? "type of item" : "different types of items") + " and `"
+//						+ invent.getTotalItemCount() + "` total items.*\n\u200B");
+//				printEntries(pageItems, eb);
+//				eb.addField("", "You have **" + maxPage + "** page" + (maxPage == 1 ? "" : "s") + " in your inventory.",
+//						false);
+////				eb.setFooter(
+////						"You have " + maxPage + " page" + (maxPage == 1 ? "" : "s") + " in your inventory.");
+//				inv.event.getChannel().sendMessageEmbeds(eb.build()).queue();
+//			}
+//			return;
+//		}
+//
+//		var entry = invent.get(type);
+//		if (entry == null)
+//			inv.event.getChannel()
+//					.sendMessage(inv.event.getAuthor().getAsMention() + " you don't have any items of that type!")
+//					.queue();
+//		else {
+//			EmbedBuilder eb = new EmbedBuilder();
+//			eb.setAuthor(inv.event.getAuthor().getAsTag() + "'s Inventory: " + entry.getName(), null,
+//					inv.event.getAuthor().getEffectiveAvatarUrl());
+//			eb.setDescription('*' + inv.event.getAuthor().getAsMention() + " has `" + entry.getTotalCount()
+//					+ "` of this item.*\n\u200B");
+//			int maxPage = JavaTools.maxPage(9, entry.getStacks());
+//			if (page > maxPage) {
+//				inv.event.getChannel().sendMessage(inv.event.getAuthor().getAsMention() + " you only have `" + maxPage
+//						+ (maxPage == 1 ? "` page" : "` pages") + " of that item in your inventory!").queue();
+//				return;
+//			}
+//			List<? extends Entry<?>.ItemStack> list = JavaTools.paginate(page, 9, entry.getStacks());
+//			printStacks(list, eb);
+//			eb.addField("", "You have **" + maxPage + "** page" + (maxPage == 1 ? "" : "s")
+//					+ " of this item in your inventory.", false);
+////			eb.setFooter("You have " + maxPage + " page" + (maxPage == 1 ? "" : "s") + " in your inventory.");
+//			inv.event.getChannel().sendMessageEmbeds(eb.build()).queue();
+//		}
 	}
 
 	private final static EmbedBuilder printEntries(List<? extends Entry<?>> entries, EmbedBuilder builder) {
