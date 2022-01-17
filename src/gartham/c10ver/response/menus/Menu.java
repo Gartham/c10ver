05c054f6b07a91a251847b67c2bf5b3246c324f7
Gartham <yorick.sanders@ttu.edu>
2022-01-17 04:42:34 -0600

    Create Menu.java

---
+++ src/gartham/c10ver/response/menus/Menu.java
@@ -0,0 +1,31 @@
+package gartham.c10ver.response.menus;
+
+import java.util.List;
+
+import gartham.c10ver.commands.InputProcessor;
+import gartham.c10ver.utils.MessageActionHandler;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
+import net.dv8tion.jda.api.interactions.components.Button;
+import net.dv8tion.jda.api.requests.restaction.MessageAction;
+
+public abstract class Menu<M> extends ButtonPaginator {
+
+	protected abstract void process(MessageAction action, List<M> items);
+
+	public class Action extends MessageActionHandler.Action {
+
+		public Action(Button button) {
+			getMah().super(button);
+		}
+
+	}
+
+	public Menu(InputProcessor<ButtonClickEvent> processor) {
+		super(processor);
+	}
+
+	public Menu(MessageActionHandler mah, InputProcessor<ButtonClickEvent> processor) {
+		super(mah, processor);
+	}
+
+}
