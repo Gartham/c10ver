96116688b18095c0ac72bb6dc91f4fa53dc922b0
Gartham <yorick.sanders@ttu.edu>
2022-01-17 04:44:33 -0600

    Update Menu.java

--- src/gartham/c10ver/response/menus/Menu.java
+++ src/gartham/c10ver/response/menus/Menu.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 import gartham.c10ver.commands.InputProcessor;
 import gartham.c10ver.utils.MessageActionHandler;
+import gartham.c10ver.utils.MessageActionHandler.Action;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
-public abstract class Menu<M> extends ButtonPaginator {
+public abstract class Menu<M extends gartham.c10ver.response.menus.Menu<M>.MenuItem> extends ButtonPaginator {
 
 	protected abstract void process(MessageAction action, List<M> items);
 
-	public class Action extends MessageActionHandler.Action {
+	public class MenuItem extends Action {
 
-		public Action(Button button) {
+		public MenuItem(Button button) {
 			getMah().super(button);
 		}
 
