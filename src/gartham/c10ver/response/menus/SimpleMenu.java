f0ba9df45008aa555921d4f50fabb8dd2531304c
Gartham <yorick.sanders@ttu.edu>
2022-01-17 18:08:05 -0600

    Update SimpleMenu.java

--- src/gartham/c10ver/response/menus/SimpleMenu.java
+++ src/gartham/c10ver/response/menus/SimpleMenu.java
@@ -5,6 +5,7 @@ import java.util.List;
 import gartham.c10ver.commands.InputProcessor;
 import gartham.c10ver.response.menus.SimpleMenu.SimpleMenuItem;
 import gartham.c10ver.utils.MessageActionHandler;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 import net.dv8tion.jda.api.interactions.components.Button;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
@@ -12,24 +13,42 @@ import net.dv8tion.jda.api.requests.restaction.MessageAction;
 public class SimpleMenu extends Menu<SimpleMenuItem> {
 
 	public SimpleMenu(InputProcessor<ButtonClickEvent> processor) {
-		super(processor);
+		super(processor, SimpleMenuItem.class);
 	}
 
 	public SimpleMenu(MessageActionHandler mah, InputProcessor<ButtonClickEvent> processor) {
-		super(mah, processor);
+		super(mah, processor, SimpleMenuItem.class);
 	}
 
 	public class SimpleMenuItem extends Menu<SimpleMenuItem>.MenuItem {
 
-		public SimpleMenuItem(Button button) {
+		private final String description;
+
+		public SimpleMenuItem(String emoji, String label, String description, String id) {
+			super(Button.secondary(id, emoji).withLabel(label));
+			this.description = description;
+		}
+
+		public SimpleMenuItem(Button button, String description) {
 			super(button);
+			this.description = description;
+		}
+
+		public String getDescription() {
+			return description;
 		}
 
 	}
 
+	protected EmbedBuilder generateEmbed(List<SimpleMenuItem> items) {
+		EmbedBuilder eb = new EmbedBuilder();
+		for (var s : items)
+			eb.addField(s.getEmoji().getAsMention() + " | " + s.getLabel(), s.getDescription(), false);
+		return eb;
+	}
+
 	@Override
 	protected void process(MessageAction action, List<SimpleMenuItem> items) {
-		// TODO Auto-generated method stub
-
+		action.setEmbeds(generateEmbed(items).build());
 	}
 }
