d61308eb3d91e0f072cc68469120dfbd39b22cf3
Gartham <gartham@dusttoash.org>
2022-01-28 18:28:27 -0600

    Update WildernessGame.java

--- src/gartham/c10ver/games/rpg/wilderness/WildernessGame.java
+++ src/gartham/c10ver/games/rpg/wilderness/WildernessGame.java
@@ -1,17 +1,20 @@
 package gartham.c10ver.games.rpg.wilderness;
 
+import java.time.Instant;
+
 import gartham.c10ver.Clover;
 import gartham.c10ver.games.rpg.wilderness.CloverWildernessMap.CloverWildernessTile;
 import gartham.c10ver.response.utils.DirectionSelector;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.GuildMessageChannel;
 import net.dv8tion.jda.api.entities.User;
 
 public class WildernessGame {
 
+	private final Instant timestamp = Instant.now();
+
 	private final User target;
-	private final TextChannel channel;
+	private final GuildMessageChannel channel;
 	private final Clover clover;
 	private final CloverWildernessMap cwm = new CloverWildernessMap();
 	private CloverWildernessTile tile = cwm.getOrigin();
@@ -22,23 +25,16 @@ public class WildernessGame {
 		return active;
 	}
 
-	public WildernessGame(User target, TextChannel channel, Clover clover) {
+	public WildernessGame(User target, GuildMessageChannel channel, Clover clover) {
 		this.target = target;
 		this.channel = channel;
 		this.clover = clover;
-	}
-
-	public void start() {
-		channel.sendMessageEmbeds(new EmbedBuilder().setDescription(tile.tilemapString())
+		channel.sendMessageEmbeds(new EmbedBuilder().setDescription("```" + tile.tilemapString() + "```")
 				.setAuthor(target.getAsTag() + "'s Exploration").build()).setActionRows(dirsel.actionRows()).complete();
 	}
 
-	public String getWildernessString() {
-		return tile.tilemapString();
-	}
-
-	public MessageEmbed getWildernessEmbed() {
-		return new EmbedBuilder().setDescription("The Wilderness:```" + getWildernessString() + "```").build();
+	public Instant getTimestamp() {
+		return timestamp;
 	}
 
 }
