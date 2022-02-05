491154c06f0323eb94d50de35a0a7019795b5368
Gartham <gartham@dusttoash.org>
2022-02-05 01:58:57 -0600

    Update CloverWildernessMap.java

--- src/gartham/c10ver/games/rpg/wilderness/CloverWildernessMap.java
+++ src/gartham/c10ver/games/rpg/wilderness/CloverWildernessMap.java
@@ -38,6 +38,7 @@ public class CloverWildernessMap extends WildernessMap<CloverWildernessMap.Clove
 		private CloverWildernessTile() {
 			super(CloverWildernessMap.this, 0, 0, DEFAULT_TILE_SIZE, DEFAULT_TILE_SIZE);
 			getGraphix().add(new ExitGraphic());
+			getGraphix().add(centerCircleGraphic(this));
 		}
 
 		private long getTileXShift() {
@@ -50,14 +51,38 @@ public class CloverWildernessMap extends WildernessMap<CloverWildernessMap.Clove
 
 	}
 
+	private static final XYLambdaRoomGraphic centerCircleGraphic(CloverWildernessTile cwt) {
+		return (x, y) -> {
+
+			// Shift to "center" by adding half of DEFAULT_TILE_SIZE, since we want the
+			// circle to be centered on the starting tile.
+			double x0 = x + cwt.getTileXShift() - DEFAULT_TILE_SIZE / 2,
+					y0 = y + cwt.getTileYShift() - DEFAULT_TILE_SIZE / 2;
+
+			double rad = Math.sqrt(x0 * x0 + y0 * y0);
+
+			if (x0 == 0 && y0 == 0 || Math.random() <= 1 - (1 / rad) * 0.7)
+				return null;
+
+			return rad < 28 ? "\uD83D\uDFEA" : null;
+
+		};
+	}
+
 	@Override
 	protected CloverWildernessTile generateTile(CloverWildernessTile from, LinkType link) {
 		Random rand = new Random(seed);
 
 		if (link instanceof AdjacencyLink) {
 			var cwt = new CloverWildernessTile(from.travel((AdjacencyLink) link));
-			cwt.getGraphix().add((XYLambdaRoomGraphic) (x,
-					y) -> x - 12 >= Math.sin(y / 3 + rand.nextDouble() * Math.PI * 2) * 3 ? "\uD83D\uDFE5" : null);
+			if (cwt.getX() < 2 && cwt.getX() > -2 && cwt.getY() < 2 && cwt.getY() > -2)
+				cwt.getGraphix().add(centerCircleGraphic(cwt));
+			else
+				cwt.getGraphix()
+						.add((XYLambdaRoomGraphic) (x,
+								y) -> x + cwt.getTileXShift() - 36 >= Math.sin((y + cwt.getTileYShift()) / 3d) * 3
+										? "\uD83D\uDFE5"
+										: null);
 			return cwt;
 		} else
 			throw new UnsupportedOperationException();
