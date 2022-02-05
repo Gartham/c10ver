e13949ff61fcb0f4236f2b6fadf5e49bebc59e5a
Gartham <gartham@dusttoash.org>
2022-02-05 00:15:26 -0600

    Update WildernessGame.java

--- src/gartham/c10ver/games/rpg/wilderness/WildernessGame.java
+++ src/gartham/c10ver/games/rpg/wilderness/WildernessGame.java
@@ -3,10 +3,15 @@ package gartham.c10ver.games.rpg.wilderness;
 import java.time.Instant;
 
 import gartham.c10ver.Clover;
+import gartham.c10ver.commands.InputProcessor;
+import gartham.c10ver.commands.consumers.InputConsumer;
 import gartham.c10ver.games.rpg.wilderness.CloverWildernessMap.CloverWildernessTile;
+import gartham.c10ver.games.rpg.wilderness.LinkType.AdjacencyLink;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.GuildMessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
 public class WildernessGame {
 
@@ -29,13 +34,47 @@ public class WildernessGame {
 		this.target = target;
 		this.channel = channel;
 		this.clover = clover;
-		channel.sendMessageEmbeds(new EmbedBuilder().setDescription("```" + tile.tilemapString() + "```")
-				.setAuthor(target.getAsTag() + "'s Exploration").build()).setActionRows(gamepad.actionRows())
-				.complete();
+		var msg = channel.sendMessageEmbeds(genEmbed()).setActionRows(gamepad.actionRows()).complete();
+		clover.getEventHandler().getButtonClickProcessor().registerInputConsumer(new InputConsumer<>() {
+
+			@Override
+			public boolean consume(ButtonClickEvent event, InputProcessor<? extends ButtonClickEvent> processor,
+					InputConsumer<ButtonClickEvent> consumer) {
+				if (event.getUser().getIdLong() != target.getIdLong() || event.getMessageIdLong() != msg.getIdLong())
+					return false;
+				switch (event.getComponentId()) {
+				case "left":
+					tile = tile.get(AdjacencyLink.LEFT);
+					break;
+				case "right":
+					tile = tile.get(AdjacencyLink.LEFT);
+					break;
+				case "up":
+					tile = tile.get(AdjacencyLink.TOP);
+					break;
+				case "down":
+					tile = tile.get(AdjacencyLink.BOTTOM);
+				default:
+					break;
+				}
+				event.editMessageEmbeds(genEmbed()).complete();
+				return true;
+			}
+		});
 	}
 
 	public Instant getTimestamp() {
 		return timestamp;
+	}
+
+	private MessageEmbed genEmbed() {
+		return new EmbedBuilder().setDescription("```" + tile.tilemapString() + "```")
+				.setAuthor(target.getAsTag() + "'s Exploration").build();
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
 	}
 
 }
