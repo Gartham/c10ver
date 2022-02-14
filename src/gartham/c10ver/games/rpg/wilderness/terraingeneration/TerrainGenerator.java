aaa4d981f430cd16e278449a5263618c05a50780
Gartham <gartham@dusttoash.org>
2022-02-14 15:35:55 -0600

    Create TerrainGenerator.java

---
+++ src/gartham/c10ver/games/rpg/wilderness/terraingeneration/TerrainGenerator.java
@@ -0,0 +1,11 @@
+package gartham.c10ver.games.rpg.wilderness.terraingeneration;
+
+public class TerrainGenerator {
+	private final int chunkWidth, chunkHeight;
+
+	public TerrainGenerator(int chunkWidth, int chunkHeight) {
+		this.chunkWidth = chunkWidth;
+		this.chunkHeight = chunkHeight;
+	}
+
+}
