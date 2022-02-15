eb80b497eef8c4a7fc26774751942a088517a04b
Gartham <gartham@dusttoash.org>
2022-02-15 15:41:49 -0600

    Update TerrainGenerator.java

--- src/gartham/c10ver/games/rpg/wilderness/terraingeneration/TerrainGenerator.java
+++ src/gartham/c10ver/games/rpg/wilderness/terraingeneration/TerrainGenerator.java
@@ -1,11 +1,34 @@
 package gartham.c10ver.games.rpg.wilderness.terraingeneration;
 
-public class TerrainGenerator {
+import java.util.ArrayList;
+import java.util.List;
+
+import gartham.c10ver.games.rpg.wilderness.Location;
+
+public abstract class TerrainGenerator<T> {
 	private final int chunkWidth, chunkHeight;
+	private final List<TerrainObjectGenerator<T>> generators = new ArrayList<>();
 
 	public TerrainGenerator(int chunkWidth, int chunkHeight) {
 		this.chunkWidth = chunkWidth;
 		this.chunkHeight = chunkHeight;
 	}
 
+	public int getChunkHeight() {
+		return chunkHeight;
+	}
+
+	public int getChunkWidth() {
+		return chunkWidth;
+	}
+	
+	public List<T> generateChunk(Location chunk) {
+		List<T> items = new ArrayList<>();
+		
+		
+		
+		return items;
+		
+	}
+	
 }
