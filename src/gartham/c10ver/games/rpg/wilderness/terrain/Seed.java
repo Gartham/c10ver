13f0efebc5033aeb0248158a957df569b4d87ed5
Gartham <gartham@dusttoash.org>
2022-02-19 02:24:37 -0600

    Create Seed.java

---
+++ src/gartham/c10ver/games/rpg/wilderness/terrain/Seed.java
@@ -0,0 +1,14 @@
+package gartham.c10ver.games.rpg.wilderness.terrain;
+
+public class Seed {
+	private final long seed;
+
+	public Seed(long seed) {
+		this.seed = seed;
+	}
+
+	public long getSeed() {
+		return seed;
+	}
+
+}
