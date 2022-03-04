5e350840bcc6834f7668353d3d6bd7e2fa003a3e
Gartham <gartham@dusttoash.org>
2022-03-03 21:16:04 -0600

    Update NoiseGenerator.java

--- src/gartham/c10ver/games/rpg/wilderness/terrain/noise/NoiseGenerator.java
+++ src/gartham/c10ver/games/rpg/wilderness/terrain/noise/NoiseGenerator.java
@@ -1,6 +1,6 @@
 package gartham.c10ver.games.rpg.wilderness.terrain.noise;
 
-import javax.tools.JavaFileManager.Location;
+import gartham.c10ver.games.rpg.wilderness.Location;
 
 public interface NoiseGenerator {
 	double[][] noisemap(Location tileLocation, int xStart, int yStart, int xEnd, int yEnd, int xSize, int ySize);
