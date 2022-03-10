8d98a190ae0f919ed297909b879ca746180b906c
Gartham <gartham@dusttoash.org>
2022-03-10 08:07:55 -0600

    Update GradGenerator.java

--- src/gartham/c10ver/games/rpg/wilderness/terrain/noise/GradGenerator.java
+++ src/gartham/c10ver/games/rpg/wilderness/terrain/noise/GradGenerator.java
@@ -1,35 +1,30 @@
 package gartham.c10ver.games.rpg.wilderness.terrain.noise;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
+import gartham.c10ver.games.rpg.random.Seed;
 import gartham.c10ver.games.rpg.wilderness.Location;
 
 interface GradGenerator {
-	Vec generate(int x, int y);
+	Vec generate(Location location, Seed seed);
 
-	static GradGenerator continuous(long seed) {
-		Random r = new Random(seed);
-		Map<Location, Vec> vectors = new HashMap<>();
-		return (x, y) -> {
-			if (vectors.containsKey(Location.of(x, y)))
-				return vectors.get(Location.of(x, y));
-			else {
-				double sqr = r.nextDouble() * 2;
-				Vec vec = new Vec((r.nextBoolean() ? -1 : 1) * Math.sqrt(sqr),
-						(r.nextBoolean() ? -1 : 1) * Math.sqrt(2 - sqr));
-				vectors.put(Location.of(x, y), vec);
-				return vec;
-			}
+	static GradGenerator continuous() {
+		return (loc, seed) -> {
+
+			// Hope the randomness isn't too crap.
+			Random r = seed.pick(loc.getX(), loc.getY()).rand();
+			double sqr = r.nextDouble() * 2;
+			Vec vec = new Vec((r.nextBoolean() ? -1 : 1) * Math.sqrt(sqr),
+					(r.nextBoolean() ? -1 : 1) * Math.sqrt(2 - sqr));
+			return vec;
 		};
 	}
 
 	Vec TL = new Vec(-1, 1), TR = new Vec(1, 1), BR = new Vec(1, -1), BL = new Vec(-1, -1);
 
-	static GradGenerator discrete(long seed) {
-		Random r = new Random(seed);
-		return (x, y) -> {
+	static GradGenerator discrete() {
+		return (loc, sed) -> {
+			var r = sed.pick(loc.getX(), loc.getY()).rand();
 			return r.nextBoolean() ? r.nextBoolean() ? TL : TR : r.nextBoolean() ? BR : BL;
 		};
 	}
