38dd8cc0dc8205c5504d865101c0b1713dac8111
Gartham <gartham@dusttoash.org>
2022-03-10 08:10:48 -0600

    Update GradGenerator.java

--- src/gartham/c10ver/games/rpg/wilderness/terrain/noise/GradGenerator.java
+++ src/gartham/c10ver/games/rpg/wilderness/terrain/noise/GradGenerator.java
@@ -5,6 +5,16 @@ import java.util.Random;
 import gartham.c10ver.games.rpg.random.Seed;
 import gartham.c10ver.games.rpg.wilderness.Location;
 
+/**
+ * A stateless <i>expression</i> that maps from a location and seed to a new
+ * {@link Vec}. {@link GradGenerator}s deterministically map every valid
+ * {@link Location} to a pseudorandomly generated {@link Vec}, with
+ * pseudorandomness manipulable through choice of seed. This type is used by
+ * {@link SmoothNoiseGenerator}s to generate perlin noise.
+ * 
+ * @author Gartham
+ *
+ */
 interface GradGenerator {
 	Vec generate(Location location, Seed seed);
 
