2a543af560b9d22d8ac7c82158d55e742a8b1538
Gartham <gartham@dusttoash.org>
2022-04-06 05:13:23 -0500

    Update Page.java

--- src/gartham/c10ver/response/buttonbox/menu/Page.java
+++ src/gartham/c10ver/response/buttonbox/menu/Page.java
@@ -1,6 +1,8 @@
 package gartham.c10ver.response.buttonbox.menu;
 
 import gartham.c10ver.response.buttonbox.ButtonBox;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.ButtonClickEvent;
 
 public interface Page {
@@ -31,4 +33,54 @@ public interface Page {
 	 * @param menu      The {@link Menu} that this {@link Page} belongs to.
 	 */
 	void update(ButtonClickEvent event, ButtonBox buttonbox, Menu menu);
+
+	static Page page(String pageContents) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setContent(pageContents).queue();
+	}
+
+	static Page page(EmbedBuilder builder) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setEmbeds(builder.build()).queue();
+	}
+
+	static Page page(String pageContents, EmbedBuilder builder) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setEmbeds(builder.build())
+				.setContent(pageContents).queue();
+	}
+
+	static Page page(EmbedBuilder... builder) {
+		return (event, buttonbox, menu) -> {
+			MessageEmbed[] me = new MessageEmbed[builder.length];
+			for (int i = 0; i < builder.length; i++)
+				me[i] = builder[i].build();
+			event.editComponents(buttonbox.rows()).setEmbeds(me).queue();
+		};
+	}
+
+	static Page page(String pageContents, EmbedBuilder... builder) {
+		return (event, buttonbox, menu) -> {
+			MessageEmbed[] me = new MessageEmbed[builder.length];
+			for (int i = 0; i < builder.length; i++)
+				me[i] = builder[i].build();
+			event.editComponents(buttonbox.rows()).setEmbeds(me).setContent(pageContents).queue();
+		};
+	}
+
+	static Page page(MessageEmbed embed) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setEmbeds(embed).queue();
+	}
+
+	static Page page(MessageEmbed... embeds) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setEmbeds(embeds).queue();
+	}
+
+	static Page page(String pageContents, MessageEmbed embed) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setEmbeds(embed)
+				.setContent(pageContents).queue();
+	}
+
+	static Page page(String pageContents, MessageEmbed... embeds) {
+		return (event, buttonbox, menu) -> event.editComponents(buttonbox.rows()).setEmbeds(embeds)
+				.setContent(pageContents).queue();
+	}
+
 }
